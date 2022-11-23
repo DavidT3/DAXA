73f0a2d3dddca91672f9fa043fc9a84d8c28e06b
DavidT3 <dt237@sussex.ac.uk>
2022-11-23 16:47:12 -0500

    Removed the instruments submodule, as I don't think that its necessary to

--- daxa/instruments/__init__.py
+++
@@ -1,2 +0,0 @@
-#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 03/11/2022, 16:04. Copyright (c) The Contributors
