c92617e73fe45a62878f339751f59d52c011aa6a
DavidT3 <dt237@sussex.ac.uk>
2022-11-23 16:23:12 -0500

    Added some config checks to config.py, and ensured that config.py is called by the top level __init__ file.

--- daxa/config.py
+++ daxa/config.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 23/11/2022, 16:08. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 23/11/2022, 16:23. Copyright (c) The Contributors
 
 import os
 from configparser import ConfigParser
@@ -33,24 +33,27 @@ if not os.path.exists(CONFIG_FILE):
     raise warn("A configuration file has been created ({}); you can use it to control where DAXA "
                "stores data by default.".format(CONFIG_FILE))
 
-# But if the config file is found, some preprocessing and checks are applied
+daxa_conf = ConfigParser()
+# It would be nice to do configparser interpolation, but it wouldn't handle the lists of energy values
+daxa_conf.read(CONFIG_FILE)
+
+if not isinstance(daxa_conf["DAXA_SETUP"]["global_archive_database"], bool):
+    raise DAXAConfigError("The value for global_archive_database must be either True or False.")
+
+# As it turns out, the ConfigParser class is a pain to work with, so we're converting to a dict here
+# Addressing works just the same
+daxa_conf = {str(sect): dict(daxa_conf[str(sect)]) for sect in daxa_conf}
+
+if daxa_conf["DAXA_SETUP"]["num_cores"] is not None:
+    # If the user has set a number of cores in the config file then we'll use that.
+    NUM_CORES = int(daxa_conf["DAXA_SETUP"]["num_cores"])
 else:
-    daxa_conf = ConfigParser()
-    # It would be nice to do configparser interpolation, but it wouldn't handle the lists of energy values
-    daxa_conf.read(CONFIG_FILE)
+    # Going to allow multi-core processing to use 90% of available cores by default, but
+    # this can be over-ridden in individual SAS calls.
+    NUM_CORES = max(int(floor(os.cpu_count() * 0.9)), 1)  # Makes sure that at least one core is used
 
-    if not isinstance(daxa_conf["DAXA_SETUP"]["global_archive_database"], bool):
-        raise DAXAConfigError("The value for global_archive_database must be either True or False.")
+# This is the default output directory for archives setup by DAXA, though it can be overridden on
+#  a mission level
+OUTPUT = os.path.abspath(daxa_conf["DAXA_SETUP"]["daxa_save_path"]) + "/"
 
-    # As it turns out, the ConfigParser class is a pain to work with, so we're converting to a dict here
-    # Addressing works just the same
-    daxa_conf = {str(sect): dict(daxa_conf[str(sect)]) for sect in daxa_conf}
-
-    if daxa_conf["DAXA_SETUP"]["num_cores"] is not None:
-        # If the user has set a number of cores in the config file then we'll use that.
-        NUM_CORES = int(daxa_conf["DAXA_SETUP"]["num_cores"])
-    else:
-        # Going to allow multi-core processing to use 90% of available cores by default, but
-        # this can be over-ridden in individual SAS calls.
-        NUM_CORES = max(int(floor(os.cpu_count() * 0.9)), 1)  # Makes sure that at least one core is used
 
