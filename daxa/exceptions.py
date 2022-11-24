e73185e61d2c3b455790e59894e38d4e59f8a08b
DavidT3 <dt237@sussex.ac.uk>
2022-11-23 20:10:47 -0500

    Added an exception for download problems

--- daxa/exceptions.py
+++ daxa/exceptions.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 23/11/2022, 15:23. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 23/11/2022, 20:10. Copyright (c) The Contributors
 
 
 class DAXAConfigError(Exception):
@@ -20,3 +20,23 @@ class DAXAConfigError(Exception):
             return '{0} '.format(self.message)
         else:
             return 'DAXAConfig has been raised'
+
+
+class DaxaDownloadError(Exception):
+    def __init__(self, *args):
+        """
+        Exception raised for problems with raw data downloads orchestrated by DAXA.
+
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{0} '.format(self.message)
+        else:
+            return 'DaxaDownloadError has been raised'
