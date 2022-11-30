af28f8003c01b58af8cd2c8be9bbbc559921afe6
DavidT3 <dt237@sussex.ac.uk>
2022-11-30 15:18:01 -0500

    Changed the missions sub-module to mission. Also added some other files to setup structure a bit

--- daxa/mission/__init__.py
+++ daxa/mission/__init__.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 08/11/2022, 13:18. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 15:17. Copyright (c) The Contributors
 
 from . import *
 
