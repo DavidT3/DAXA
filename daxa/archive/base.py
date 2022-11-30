c355da457e1d6b3161a2a5285453bd6a6510c3b4
DavidT3 <dt237@sussex.ac.uk>
2022-11-30 17:24:34 -0500

    Added some 'special' Python methods to the Archive class - specifically for iterating through the associated missions, deleting data, and accessing missions by indexing the Archive.

--- daxa/archive/base.py
+++ daxa/archive/base.py
@@ -1,7 +1,9 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 17:01. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 17:24. Copyright (c) The Contributors
 import os
 from typing import List, Union
+
+import numpy as np
 
 from daxa import BaseMission, OUTPUT
 from daxa.exceptions import DuplicateMissionError, ArchiveExistsError
@@ -42,7 +44,7 @@ class Archive:
 
         self._archive_path = OUTPUT + 'archives/' + archive_name + '/'
 
-        self._missions = missions
+        self._missions = {m.name: m for m in missions}
 
     # Defining properties first
     @property
@@ -83,3 +85,78 @@ class Archive:
         :rtype: List[str]
         """
         return [m.name for m in self._missions]
+
+    @property
+    def missions(self) -> Union[List[BaseMission], BaseMission]:
+        """
+        Property getter that returns either a list of missions associated with this Archive, or a single
+        mission associated with this Archive (if only one mission was supplied).
+
+        :return: Missions (or mission) associated with this archive.
+        :rtype: Union[List[BaseMission], BaseMission]
+        """
+        if len(self._missions) == 1:
+            return list(self._missions.values())[0]
+        else:
+            return list(self._missions.values())
+
+    # Define the 'special' Python methods
+    def __len__(self):
+        """
+        The result of using the Python len() command on this archive - the number of missions associated.
+
+        :return: The number of missions in this archive.
+        :rtype: int
+        """
+        return len(self._missions)
+
+    def __iter__(self):
+        """
+        Called when initiating iterating through an archive. Resets the counter _n.
+        """
+        self._n = 0
+        return self
+
+    def __next__(self):
+        """
+        Iterates the counter _n uses it to find the name of the corresponding mission, then retrieves
+        that source from the _missions dictionary. Missions are accessed using their name as a key.
+        """
+        if self._n < self.__len__():
+            result = self.__getitem__(self.mission_names[self._n])
+            self._n += 1
+            return result
+        else:
+            raise StopIteration
+
+    def __getitem__(self, key: Union[int, str]) -> BaseMission:
+        """
+        This returns the relevant mission when an archive is addressed using the name of a mission as
+        the key, or using an integer index.
+
+        :param int/str key: The index or name of the mission to fetch.
+        :return: The relevant mission object.
+        :rtype: BaseMission
+        """
+        if isinstance(key, (int, np.integer)):
+            mission = self._missions[self.mission_names[key]]
+        elif isinstance(key, str):
+            mission = self._missions[key]
+        else:
+            mission = None
+            raise ValueError("Only a source name or integer index may be used to address an archive object")
+        return mission
+
+    def __delitem__(self, key: Union[int, str]):
+        """
+        This deletes a mission from the archive.
+
+        :param int/str key: The index or name of the mission to delete.
+        """
+        if isinstance(key, (int, np.integer)):
+            del self._missions[self.mission_names[key]]
+        elif isinstance(key, str):
+            del self._missions[key]
+        else:
+            raise ValueError("Only a mission name or integer index may be used to address an archive object")
+
