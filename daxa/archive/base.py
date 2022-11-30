5cb85b50fac168b245fd72aad58e4f5ae991e480
DavidT3 <dt237@sussex.ac.uk>
2022-11-30 15:30:38 -0500

    Setup the archive submodule with some files added to create the structure - base will contain the Archive class (issue #19), assemble will contain convenience functions for easily assembling archives in specific circumstances.

--- daxa/archive/base.py
+++ daxa/archive/base.py
@@ -1,2 +1,2 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 23/11/2022, 16:53. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 15:30. Copyright (c) The Contributors
