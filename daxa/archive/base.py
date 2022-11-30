a89ccb7844f4bfb043cde06c8c65cfcff687fc8d
DavidT3 <dt237@sussex.ac.uk>
2022-11-30 17:02:01 -0500

    Added the bare-bones of an Archive class

--- daxa/archive/base.py
+++ daxa/archive/base.py
@@ -1,2 +1,85 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 15:30. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 17:01. Copyright (c) The Contributors
+import os
+from typing import List, Union
+
+from daxa import BaseMission, OUTPUT
+from daxa.exceptions import DuplicateMissionError, ArchiveExistsError
+
+
+class Archive:
+    def __init__(self, missions: Union[List[BaseMission], BaseMission], archive_name: str):
+        """
+
+        :param missions:
+        :param str archive_name: The name
+        """
+        # First ensure that the missions variable is iterable even if there's only one missions that has
+        #  been passed, makes it easier to generalise things.
+        if isinstance(missions, BaseMission):
+            missions = [missions]
+        elif not isinstance(missions, list):
+            raise TypeError("Please pass either a single missions class instance, or a list of missions class "
+                            "instances to the 'missions' argument.")
+
+        miss_names = [m.name for m in missions]
+        if len(miss_names) != len(list(set(miss_names))):
+            raise DuplicateMissionError("There are multiple instances of the same missions present in "
+                                        "the 'missions' argument - only one instance of each is allowed for "
+                                        "a particular archive.")
+
+        self._archive_name = archive_name
+
+        if not os.path.exists(OUTPUT + 'archives/' + archive_name):
+            os.makedirs(OUTPUT + 'archives/' + archive_name)
+        else:
+            raise ArchiveExistsError("An archive named {an} already exists in the output directory "
+                                     "({od}).".format(an=archive_name, od=OUTPUT + 'archives/'))
+
+        # TODO maybe check for the existence of some late-stage product/file to see whether the archive
+        #  has already been successfully generated
+        # elif os.path.exists(OUTPUT + archive_name + '/')
+
+        self._archive_path = OUTPUT + 'archives/' + archive_name + '/'
+
+        self._missions = missions
+
+    # Defining properties first
+    @property
+    def archive_name(self) -> str:
+        """
+        Property getter for the name assigned to this archive by the user.
+        :return: The archive name.
+        :rtype: str
+        """
+        return self._archive_name
+
+    @property
+    def top_level_path(self) -> str:
+        """
+        The property getter for the absolute path to the top-level DAXA storage directory.
+
+        :return: Absolute top-level storage path.
+        :rtype: str
+        """
+        return OUTPUT
+
+    @property
+    def archive_path(self) -> str:
+        """
+        The property getter for the absolute path to the output archive directory.
+
+        :return: Absolute path to the archive.
+        :rtype: str
+        """
+        return self._archive_path
+
+    @property
+    def mission_names(self) -> List[str]:
+        """
+        Property getter for the names of the missions associated with this Archive.
+
+        :return:
+        :rtype: List[str]
+        """
+        return [m.name for m in self._missions]
