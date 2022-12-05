b814dfc15462ad0423aeb2a17adc85064590dc95
DavidT3 <dt237@sussex.ac.uk>
2022-12-05 14:30:07 -0500

    Updated the find_sas function to enforce a minimum version requirement on SAS, uses the packaging module version parsing capabilities.

--- daxa/process/_backend_check.py
+++ daxa/process/_backend_check.py
@@ -1,20 +1,23 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 05/12/2022, 10:22. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 05/12/2022, 14:30. Copyright (c) The Contributors
 
 import os
 from subprocess import Popen, PIPE
 
-from ..exceptions import SASNotFoundError
+from packaging.version import Version
+
+from ..exceptions import SASNotFoundError, SASVersionError
 
 
-def find_sas() -> str:
+def find_sas() -> Version:
     """
     This function checks to ensure the presence of SAS on the host system, and it will be called before performing
     any data processing/reduction of XMM data. An error will be thrown if SAS (or SAS calibration files) cannot
     be identified on the system.
 
-    :return: The SAS version that has been successfully identified.
-    :rtype: float
+    :return: The SAS version that has been successfully identified, as an instance of the 'packaging'
+        module's Version class.
+    :rtype: Version
     """
     # Here we check to see whether SAS is installed (along with all the necessary paths)
     sas_version = None
@@ -25,7 +28,7 @@ def find_sas() -> str:
         sas_avail = False
     else:
         sas_out, sas_err = Popen("sas --version", stdout=PIPE, stderr=PIPE, shell=True).communicate()
-        sas_version = sas_out.decode("UTF-8").strip("]\n").split('-')[-1]
+        sas_version = Version(sas_out.decode("UTF-8").strip("]\n").split('-')[-1])
         sas_avail = True
 
     # This checks for the CCF path, which is required to use cifbuild, which is required to do basically
@@ -33,5 +36,9 @@ def find_sas() -> str:
     if sas_avail and "SAS_CCFPATH" not in os.environ:
         raise SASNotFoundError("SAS_CCFPATH environment variable is not set, this is required to generate "
                                "calibration files. As such XMM raw data (ODFs) cannot be processed.")
+
+    if sas_version < Version('14.0.0'):
+        raise SASVersionError("The detected SAS installation is of too low a version ({v}), please use version 14 or "
+                              "later.".format(v=sas_version))
 
     return sas_version
