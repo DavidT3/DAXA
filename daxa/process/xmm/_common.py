284576f7356c251e324117e714dba257330d116b
DavidT3 <dt237@sussex.ac.uk>
2022-12-07 14:25:31 -0500

    Hopefully finalized the first version of the SAS decorator to run SAS tasks

--- daxa/process/xmm/_common.py
+++ daxa/process/xmm/_common.py
@@ -1,10 +1,16 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 06/12/2022, 17:08. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 07/12/2022, 14:25. Copyright (c) The Contributors
 
 import os.path
+from functools import wraps
+from multiprocessing.dummy import Pool
+from subprocess import Popen, PIPE
+from typing import Tuple
 from warnings import warn
 
+from exceptiongroup import ExceptionGroup
 from packaging.version import Version
+from tqdm import tqdm
 
 from daxa.archive.base import Archive
 from daxa.exceptions import NoXMMMissionsError
@@ -55,181 +61,154 @@ def _sas_process_setup(obs_archive: Archive) -> Version:
     return sas_vers
 
 
-# def execute_cmd(cmd: str, p_type: str, p_path: list, extra_info: dict, src: str) -> Tuple[BaseProduct, str]:
-#     """
-#
-#
-#     :param str cmd: SAS command to be executed on the command line.
-#     :param str p_type: The product type that will be produced by this command.
-#     :param str p_path: The final output path of the product.
-#     :param dict extra_info: Any extra information required to define the product object.
-#     :param str src: A string representation of the source object that this product is associated with.
-#     :return: The product object, and the string representation of the associated source object.
-#     :rtype: Tuple[BaseProduct, str]
-#     """
-#     out, err = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
-#     out = out.decode("UTF-8", errors='ignore')
-#     err = err.decode("UTF-8", errors='ignore')
-#
-#
-#
-#     return prod, src
+def execute_cmd(cmd: str, rel_id: str, miss_name: str, check_path: str) -> Tuple[str, str, bool, str, str]:
+    """
+    This is a simple function designed to execute cmd line SAS commands for the processing and reduction of
+    XMM mission data. It will collect the stdout and stderr values for each command and return them too for the
+    process of logging. Finally, it checks that a specified 'final file' actually exists at the expected path, as
+    a final check of the success of whatever process has been run.
+
+    :param str cmd: The command that should be executed in a bash shell.
+    :param str rel_id: Whatever ID has been attached to the particular command (it could be an ObsID, or an ObsID
+        + instrument combination depending on the task).
+    :param str miss_name: The specific XMM mission name that this task belongs to.
+    :param str check_path: The path where a 'final file' should exist, used for the purposes of checking
+        that it exists.
+    :return: The rel_id, a boolean flag indicating whether the final file exists, the std_out, and the std_err.
+    :rtype: Tuple[str, str, bool, str, str]
+    """
+
+    # Starts the process running on a shell, connects to the process and waits for it to terminate, and collects
+    #  the stdout and stderr
+    out, err = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+    # Decodes the stdout and stderr from the binary encoding it currently exists in. The errors='ignore' flag
+    #  means that it doesn't throw errors if there is a character it doesn't recognize
+    out = out.decode("UTF-8", errors='ignore')
+    err = err.decode("UTF-8", errors='ignore')
+
+    # Simple check on whether the 'final file' passed into this function actually exists or not
+    if os.path.exists(check_path):
+        file_exists = True
+    else:
+        file_exists = False
+
+    return rel_id, miss_name, file_exists, out, err
 
 
-# def sas_call(sas_func):
-#     """
-#     This is used as a decorator for functions that produce SAS command strings.
-#     """
-#
-#     @wraps(sas_func)
-#     def wrapper(*args, **kwargs):
-#
-#         # This is the output from whatever function this is a decorator for
-#         cmd_list, to_stack, to_execute, cores, p_type, paths, extra_info, disable = sas_func(*args, **kwargs)
-#
-#         src_lookup = {}
-#         all_run = []  # Combined command list for all sources
-#         all_type = []  # Combined expected type list for all sources
-#         all_path = []  # Combined expected path list for all sources
-#         all_extras = []  # Combined extra information list for all sources
-#         source_rep = []  # For repr calls of each source object, needed for assigning products to sources
-#         for ind in range(len(cmd_list)):
-#             source = sources[ind]
-#             if len(cmd_list[ind]) > 0:
-#                 src_lookup[repr(source)] = ind
-#                 # If there are commands to add to a source queue, then do it
-#                 source.update_queue(cmd_list[ind], p_type[ind], paths[ind], extra_info[ind], to_stack)
-#
-#             # If we do want to execute the commands this time round, we read them out for all sources
-#             # and add them to these master lists
-#             if to_execute:
-#                 to_run, expected_type, expected_path, extras = source.get_queue()
-#                 all_run += to_run
-#                 all_type += expected_type
-#                 all_path += expected_path
-#                 all_extras += extras
-#                 source_rep += [repr(source)] * len(to_run)
-#
-#         # This is what the returned products get stored in before they're assigned to sources
-#         results = {s: [] for s in src_lookup}
-#         # Any errors raised shouldn't be SAS, as they are stored within the product object.
-#         raised_errors = []
-#         # Making sure something is defined for this variable
-#         prod_type_str = ""
-#         if to_execute and len(all_run) > 0:
-#             # Will run the commands locally in a pool
-#             prod_type_str = ", ".join(set(all_type))
-#             with tqdm(total=len(all_run), desc="Generating products of type(s) " + prod_type_str,
-#                       disable=disable) as gen, Pool(cores) as pool:
-#                 def callback(results_in: Tuple[BaseProduct, str]):
-#                     """
-#                     Callback function for the apply_async pool method, gets called when a task finishes
-#                     and something is returned.
-#                     :param Tuple[BaseProduct, str] results_in: Results of the command call.
-#                     """
-#                     nonlocal gen  # The progress bar will need updating
-#                     nonlocal results  # The dictionary the command call results are added to
-#                     if results_in[0] is None:
-#                         gen.update(1)
-#                         return
-#                     else:
-#                         prod_obj, rel_src = results_in
-#                         results[rel_src].append(prod_obj)
-#                         gen.update(1)
-#
-#                 def err_callback(err):
-#                     """
-#                     The callback function for errors that occur inside a task running in the pool.
-#                     :param err: An error that occurred inside a task.
-#                     """
-#                     nonlocal raised_errors
-#                     nonlocal gen
-#
-#                     if err is not None:
-#                         # Rather than throwing an error straight away I append them all to a list for later.
-#                         raised_errors.append(err)
-#                     gen.update(1)
-#
-#                 for cmd_ind, cmd in enumerate(all_run):
-#                     # These are just the relevant entries in all these lists for the current command
-#                     # Just defined like this to save on line length for apply_async call.
-#                     exp_type = all_type[cmd_ind]
-#                     exp_path = all_path[cmd_ind]
-#                     ext = all_extras[cmd_ind]
-#                     src = source_rep[cmd_ind]
-#                     pool.apply_async(execute_cmd, args=(str(cmd), str(exp_type), exp_path, ext, src),
-#                                      error_callback=err_callback, callback=callback)
-#                 pool.close()  # No more tasks can be added to the pool
-#                 pool.join()  # Joins the pool, the code will only move on once the pool is empty.
-#
-#         elif to_execute and len(all_run) == 0:
-#             # It is possible to call a wrapped SAS function and find that the products already exist.
-#             # print("All requested products already exist")
-#             pass
-#
-#         # Now we assign products to source objects
-#         all_to_raise = []
-#         # This is for the special case of generating an AnnularSpectra product
-#         ann_spec_comps = {k: [] for k in results}
-#         for entry in results:
-#             # Made this lookup list earlier, using string representations of source objects.
-#             # Finds the ind of the list of sources that we should add this set of products to
-#             ind = src_lookup[entry]
-#             to_raise = []
-#             for product in results[entry]:
-#                 product: BaseProduct
-#                 ext_info = " {s} is the associated source, the specific data used is " \
-#                            "{o}-{i}.".format(s=sources[ind].name, o=product.obs_id, i=product.instrument)
-#                 if len(product.sas_errors) == 1:
-#                     to_raise.append(SASGenerationError(product.sas_errors[0] + ext_info))
-#                 elif len(product.sas_errors) > 1:
-#                     errs = [SASGenerationError(e + ext_info) for e in product.sas_errors]
-#                     to_raise += errs
-#
-#                 if len(product.errors) == 1:
-#                     to_raise.append(SASGenerationError(product.errors[0] + "-" + ext_info))
-#                 elif len(product.errors) > 1:
-#                     errs = [SASGenerationError(e + "-" + ext_info) for e in product.errors]
-#                     to_raise += errs
-#
-#                 # ccfs aren't actually stored in the source product storage, but they are briefly put into
-#                 #  BaseProducts for error parsing etc. So if the product type is None we don't store it
-#                 if product.type is not None and product.usable and prod_type_str != "annular spectrum set components":
-#                     # For each product produced for this source, we add it to the storage hierarchy
-#                     sources[ind].update_products(product)
-#                 elif product.type is not None and product.usable and prod_type_str == "annular spectrum set components":
-#                     # Really we're just re-creating the results dictionary here, but I want these products
-#                     #  to go through the error checking stuff like everything else does
-#                     ann_spec_comps[entry].append(product)
-#
-#             if len(to_raise) != 0:
-#                 all_to_raise.append(to_raise)
-#
-#         if prod_type_str == "annular spectrum set components":
-#             for entry in ann_spec_comps:
-#                 # So now we pass the list of spectra to a AnnularSpectra definition - and it will sort them out
-#                 #  itself so the order doesn't matter
-#                 ann_spec = AnnularSpectra(ann_spec_comps[entry])
-#                 # Refresh the value of ind so that the correct source is used for radii conversion and so that
-#                 #  the AnnularSpectra is added to the correct source.
-#                 ind = src_lookup[entry]
-#                 if sources[ind].redshift is not None:
-#                     # If we know the redshift we will add the radii to the annular spectra in proper distance units
-#                     ann_spec.proper_radii = sources[ind].convert_radius(ann_spec.radii, 'kpc')
-#                 # And adding our exciting new set of annular spectra into the storage structure
-#                 sources[ind].update_products(ann_spec)
-#
-#         # Errors raised here should not be to do with SAS generation problems, but other purely pythonic errors
-#         for error in raised_errors:
-#             raise error
-#
-#         # And here are all the errors during SAS generation, if any
-#         if len(all_to_raise) != 0:
-#             raise SASGenerationError(all_to_raise)
-#
-#         # If only one source was passed, turn it back into a source object rather than a source
-#         # object in a list.
-#         if len(sources) == 1:
-#             sources = sources[0]
-#         return sources
-#     return wrapper
+def sas_call(sas_func):
+    """
+    This is used as a decorator for functions that produce SAS command strings.
+    """
+
+    @wraps(sas_func)
+    def wrapper(*args, **kwargs):
+
+        # The first argument of all the SAS processing functions will be an archive instance, and pulling
+        #  that out of the arguments will be useful later
+        obs_archive = args[0]
+
+        # This is the output from whatever function this is a decorator for
+        miss_cmds, miss_final_paths, miss_extras, process_message, cores, disable = sas_func(*args, **kwargs)
+
+        # This just sets up a dictionary of how many tasks there are for each mission
+        num_to_run = {mn: len(miss_cmds[mn]) for mn in miss_cmds}
+
+        # The first dictionary is to store boolean flags for each task, True if they succeeded (i.e. no errors +
+        #  the final file exists), False if they didn't. The second dictionary is to store raised errors. The
+        #  top level keys are mission names, the lower level keys are whatever was used for the task being run (i.e.
+        #  either ObsID or ObsID+Inst, depending on the task).
+        success_flags = {}
+        process_stderrs = {}
+        # The std outs recorded for each task, keys are the same as the two dictionaries above
+        process_stdouts = {}
+        # I do not love this solution, but this will be what any python errors that are thrown during execute_cmd
+        #  are stored in. In theory, because execute_cmd is so simple, there shouldn't be Python errors thrown.
+        #  SAS errors will be stored in process_stderrs
+        python_errors = []
+
+        # Iterating through the missions (there may only one but as the dictionary will have mission name as the top
+        #  level key regardless this is valid for one or multiple XMM missions).
+        for miss_name in miss_cmds:
+            # Set up top level (mission name) keys for the output storage dictionaries
+            success_flags[miss_name] = {}
+            process_stderrs[miss_name] = {}
+            process_stdouts[miss_name] = {}
+
+            # There's no point setting up a Pool etc. if there are no tasks to run for the current mission, so
+            #  we check how many there are
+            if num_to_run[miss_name] > 0:
+                # Use the mission name to grab the relevant mission object out from the observation archive
+                rel_miss = obs_archive[miss_name]
+
+                # Set up a tqdm progress bar, as well as a Pool for multiprocessing (using the number of cores
+                #  specified in the SAS task that this decorator wraps. We want to parallelize these tasks because
+                #  they tend to be embarrassingly parallelise
+                with tqdm(total=num_to_run[miss_name], desc=rel_miss.pretty_name + ' - ' + process_message,
+                          disable=disable) as gen, Pool(cores) as pool:
+
+                    # This 'callback' function is triggered when the parallelized function completes successfully
+                    #  and returns.
+                    def callback(results_in: Tuple[str, str, bool, str, str]):
+                        """
+                        Callback function for the apply_async pool method, gets called when a task finishes
+                        and something is returned.
+
+                        :param Tuple[str, str, bool, str, str] results_in: The output of execute_cmd.
+                        """
+                        # The progress bar will need updating
+                        nonlocal gen
+                        # Need to make sure we have access to these dictionaries to store information on process
+                        #  success, stderr, and stdout
+                        nonlocal success_flags
+                        nonlocal process_stderrs
+                        nonlocal process_stdouts
+
+                        # Just unpack the results in for clarity's sake
+                        relevant_id, mission_name, does_file_exist, proc_out, proc_err = results_in
+
+                        # We consider the task successful if the final file exists and there is nothing
+                        #  in the stderr output
+                        if does_file_exist and len(proc_err) == 0:
+                            success_flags[mission_name][relevant_id] = True
+                        else:
+                            success_flags[mission_name][relevant_id] = False
+                            process_stderrs[mission_name][relevant_id] = proc_err
+
+                        # Store the stdout for logging purposes
+                        process_stdouts[mission_name][relevant_id] = proc_out
+
+                        # Make sure to update the progress bar
+                        gen.update(1)
+
+                    # This other 'callback' function is triggered when Python inside the parallelised function
+                    #  raises an exception rather than completing successfully
+                    def err_callback(err):
+                        """
+                        The callback function for errors that occur inside a task running in the pool.
+                        :param err: An error that occurred inside a task.
+                        """
+                        nonlocal python_errors
+                        nonlocal gen
+
+                        if err is not None:
+                            # Rather than throwing an error straight away I append them all to a list for later.
+                            python_errors.append(err)
+                        gen.update(1)
+
+                    for rel_id, cmd in miss_cmds[miss_name].items():
+                        rel_fin_path = miss_final_paths[miss_name][rel_id]
+
+                        pool.apply_async(execute_cmd, args=(cmd, rel_id, miss_name, rel_fin_path),
+                                         error_callback=err_callback, callback=callback)
+                    pool.close()  # No more tasks can be added to the pool
+                    pool.join()  # Joins the pool, the code will only move on once the pool is empty.
+
+            # This uses the new ExceptionGroup class to raise a set of python errors (if there are any raised
+            #  during the execute_cmd function calls)
+            if len(python_errors) != 0:
+                raise ExceptionGroup("Python errors raised during SAS commands", python_errors)
+
+        return success_flags, process_stderrs, process_stdouts
+    return wrapper
+
+
