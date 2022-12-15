0c7217a0d02e1fdbf6a6b61a5e24ceaa1acdf08c
DavidT3 <dt237@sussex.ac.uk>
2022-12-15 13:00:39 -0500

    Added an error to be raised when a required processing step has not been run

--- daxa/exceptions.py
+++ daxa/exceptions.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 07/12/2022, 15:46. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 15/12/2022, 13:00. Copyright (c) The Contributors
 
 
 class DAXAConfigError(Exception):
@@ -183,3 +183,23 @@ class NoProcessingError(Exception):
             return '{0} '.format(self.message)
         else:
             return 'NoProcessingError has been raised'
+
+
+class NoDependencyProcessError(Exception):
+    def __init__(self, *args):
+        """
+        Exception raised if a processing method that the current process depends on has not been run.
+
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{0} '.format(self.message)
+        else:
+            return 'NoDependencyProcessError has been raised'
