bdc01ab230ff057cda312d080c7af4c2c708f466
DavidT3 <dt237@sussex.ac.uk>
2022-12-16 14:35:47 -0500

    Made the parameter checking for espfilt a bit nicer, and made sure that all values to identify if smoothing/binning are to be turned on are properly set. For issue #48

--- daxa/process/xmm/clean.py
+++ daxa/process/xmm/clean.py
@@ -1,5 +1,7 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 16/12/2022, 13:42. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 16/12/2022, 14:35. Copyright (c) The Contributors
+import os
+from random import randint
 from typing import Union
 from warnings import warn
 
@@ -11,11 +13,11 @@ from daxa.exceptions import NoDependencyProcessError
 from daxa.process.xmm._common import _sas_process_setup, ALLOWED_XMM_MISSIONS
 
 
-def espfilt(obs_archive: Archive, method: str = 'histogram', with_smoothing: Union[None, bool, Quantity] = False,
-            with_binning: Union[None, bool, Quantity] = False, ratio: float = 1.2,
-            lo_en: Quantity = Quantity(2500, 'eV'), hi_en: Quantity = Quantity(8000, 'eV'), range_scale: dict = None,
-            allowed_sigma: float = 2.5, gauss_fit_lims: tuple = (0.1, 6.5), num_cores: int = NUM_CORES,
-            disable_progress: bool = False):
+def espfilt(obs_archive: Archive, method: str = 'histogram', with_smoothing: Union[bool, Quantity] = False,
+            with_binning: Union[bool, Quantity] = False, ratio: float = 1.2, lo_en: Quantity = Quantity(2500, 'eV'),
+            hi_en: Quantity = Quantity(8000, 'eV'), range_scale: dict = None, allowed_sigma: float = 2.5,
+            gauss_fit_lims: tuple = (0.1, 6.5), num_cores: int = NUM_CORES, disable_progress: bool = False):
+
     # Run the setup for SAS processes, which checks that SAS is installed, checks that the archive has at least
     #  one XMM mission in it, and shows a warning if the XMM missions have already been processed
     sas_version = _sas_process_setup(obs_archive)
@@ -24,50 +26,64 @@ def espfilt(obs_archive: Archive, method: str = 'histogram', with_smoothing: Uni
     if method != 'histogram' and method != 'ratio':
         raise ValueError("The string passed for 'method' must be either histogram or ratio.")
 
-    # Parsing the user's choice of parameter for espfilt smoothing
-    if isinstance(with_smoothing, bool) and with_smoothing:
+    # Parsing the user's choice of parameters for espfilt smoothing
+    if not isinstance(with_smoothing, (bool, Quantity)):
+        raise TypeError("The with_smoothing parameter must be either boolean or an astropy quantity.")
+    # If with_smoothing is boolean then no custom value for smooth_factor has been passed, so we use the
+    #  espfilt default
+    elif isinstance(with_smoothing, bool):
         smooth_factor = Quantity(51, 's')
-    elif isinstance(with_smoothing, bool) and not with_smoothing:
-        smooth_factor = None
-    elif with_smoothing is None:
-        smooth_factor = None
-    elif isinstance(with_smoothing, Quantity) and not with_smoothing.unit.is_equivalent('s'):
+    # If with_smoothing wasn't boolean then it must be a Quantity - so must check the units
+    elif not with_smoothing.unit.is_equivalent('s'):
         raise UnitConversionError("If the 'with_smoothing' argument is an Astropy Quantity, it must be in units that"
                                   " can be converted to seconds.")
+    # Ensure that the value is within the allowed limits specified in the espfilt documentation
     elif with_smoothing < Quantity(1, 's') or with_smoothing > Quantity(60, 's'):
         raise ValueError("If the with_smoothing argument is set (to activate light-curve smoothing) then it must be "
                          "set to a value greater than 1 second and less than 60 seconds.")
     elif isinstance(with_smoothing, Quantity):
         smooth_factor = with_smoothing.to('s')
+        # If a custom smoothing factor has been passed then the smoothing will be turned on
+        with_smoothing = True
 
     # Parsing the user's choice of parameters for espfilt binning
-    if isinstance(with_binning, bool) and with_binning:
+    if not isinstance(with_binning, (bool, Quantity)):
+        raise TypeError("The with_binning parameter must be either boolean or an astropy quantity.")
+    # If with_binning is boolean then no custom value for bin_size has been passed, so we use the espfilt default
+    elif isinstance(with_binning, bool):
         bin_size = Quantity(60, 's')
-    elif isinstance(with_binning, bool) and not with_binning:
-        bin_size = None
-    elif with_binning is None:
-        bin_size = None
-    elif isinstance(with_binning, Quantity) and not with_binning.unit.is_equivalent('s'):
+    # If with_binning wasn't boolean then it must be a Quantity - so must check the units
+    elif not with_binning.unit.is_equivalent('s'):
         raise UnitConversionError("If the 'with_binning' argument is an Astropy Quantity, it must be in units that"
                                   " can be converted to seconds.")
-    elif isinstance(with_binning, Quantity) and with_binning < Quantity(1, 's'):
+    # Ensure that the value is within the allowed limits specified in the espfilt documentation
+    elif with_binning < Quantity(1, 's'):
         raise ValueError("If the with_binning argument is set (to activate light-curve binning) then it must be set "
                          "to a value greater than 1 second.")
     elif isinstance(with_binning, Quantity):
         bin_size = with_binning.to('s')
+        # If a custom bin size has been passed then the binning will be turned on
+        with_binning = True
 
+    # The only check we make on the flaring ratio of annulus counts is to ensure it's an integer/float - no limits
+    #  were specified in the parameter docs of espfilt
     if not isinstance(ratio, (int, float)):
         raise TypeError("The ratio argument must be either a float or an integer.")
 
+    # Have to make sure that the energy bounds are in units that can be converted to eV (which is what espfilt
+    #  expects for these arguments).
     if not lo_en.unit.is_equivalent('eV') or not hi_en.unit.is_equivalent('eV'):
         raise UnitConversionError("The lo_en and hi_en arguments must be astropy quantities in units that can be "
                                   "converted to eV.")
-    elif lo_en <= hi_en:
+    # Obviously the upper limit can't be lower than the lower limit, or equal to it.
+    elif hi_en <= lo_en:
         raise ValueError("The hi_en argument must be larger than the lo_en argument.")
+    # Make sure we're converted to the right unit
     else:
         lo_en = lo_en.to('eV')
         hi_en = hi_en.to('eV')
 
+    # Also enforce the value limits specified in the espfilt documentation
     if (lo_en < Quantity(1, 'eV') or lo_en > Quantity(32766, 'eV')) or \
             (hi_en < Quantity(2, 'eV') or hi_en > Quantity(32767, 'eV')):
         raise ValueError("The lo_en value must be greater than 1 eV and less than 32766 eV, the hi_en value must be "
@@ -75,18 +91,26 @@ def espfilt(obs_archive: Archive, method: str = 'histogram', with_smoothing: Uni
 
     # Setting the default values for range scale, can't have a mutable argument
     if range_scale is None:
+        # These are the default values from the parameter docs of espfilt, for MOS and PN
         range_scale = {'mos': 6.0, 'pn': 15.0}
+    # Have to make sure that if the user isn't using the defaults, they have passed the data in the form that we
+    #  expect it to be - don't want to pass dodgy parameters to espfilt as it is better for Python exceptions to
+    #  be called than bash errors to pop up.
     elif range_scale is not None and not isinstance(range_scale, dict):
         raise ValueError("The range_scale argument must be a dictionary with an entry for MOS instruments (with key "
                          "'mos') and an entry for the PN instrument (with key 'pn').")
-    elif range_scale is not None and isinstance(range_scale, dict) and (
-            'pn' not in range_scale or 'mos' not in range_scale):
+    # Make sure that there is an entry for MOS and PN if the user has passed their own
+    elif range_scale is not None and isinstance(range_scale, dict) and \
+            ('pn' not in range_scale or 'mos' not in range_scale):
         raise KeyError("The range_scale argument must be a dictionary with an entry for MOS instruments (with key "
                        "'mos') and an entry for the PN instrument (with key 'pn').")
 
+    # The only check we make on the limit in sigma for unflared rates is to ensure it's an integer/float - no limits
+    #  were specified in the parameter docs of espfilt
     if not isinstance(allowed_sigma, (int, float)):
         raise TypeError("The allowed_sigma argument must be either an integer or a float.")
 
+    # This should be a tuple with two int/float entries, these checks make sure of that
     if not isinstance(gauss_fit_lims, tuple):
         raise TypeError("The gauss_fit_lims argument must be a tuple.")
     elif len(gauss_fit_lims) != 2:
@@ -94,6 +118,13 @@ def espfilt(obs_archive: Archive, method: str = 'histogram', with_smoothing: Uni
                          "the upper limit.")
     elif not all([isinstance(g_lim, (float, int)) for g_lim in gauss_fit_lims]):
         raise TypeError("The elements of gauss_fit_lims must be either integers or floats.")
+    # The upper limit cannot be less or equal to the lower limit, it wouldn't make sense
+    elif gauss_fit_lims[1] <= gauss_fit_lims[0]:
+        raise ValueError("The second (upper) entry in the gauss_fit_lims argument cannot be less than or equal to"
+                         " the first (lower) entry.")
+    # Finally, we inflict the value limits specified in the espfilt parameter docs
+    elif any([g_lim < 0 or g_lim > 10 for g_lim in gauss_fit_lims]):
+        raise ValueError("The entries in gauss_fit_lims must be greater than zero, and less than 10.")
 
     # Define the form of the emchain command that must be run to check for anomalous states in MOS CCDs
     ef_cmd = "cd {d}; export SAS_CCF={ccf}; espfilt eventfile={ef} withoot={woot} ootfile={oot} method={me} " \
@@ -146,13 +177,15 @@ def espfilt(obs_archive: Archive, method: str = 'histogram', with_smoothing: Uni
             if 'M1' in val_id:
                 obs_id, exp_id = val_id.split('M1')
                 inst = 'M1'
+                evt_list_file = obs_archive.process_extra_info[miss.name]['emchain'][val_id]['evt_list']
+                oot_evt_list_file = 'dataset'
             elif 'M2' in val_id:
                 obs_id, exp_id = val_id.split('M2')
                 # The form of this inst is different to the standard in DAXA/SAS (M2), because emanom log files
                 #  are named with mos1 and mos2 rather than M1 and M2
                 inst = 'M2'
                 evt_list_file = obs_archive.process_extra_info[miss.name]['emchain'][val_id]['evt_list']
-                oot_evt_list_file = None
+                oot_evt_list_file = 'dataset'
             elif 'PN' in val_id:
                 obs_id, exp_id = val_id.split('PN')
                 # The form of this inst is different to the standard in DAXA/SAS (M2), because emanom log files
@@ -164,38 +197,51 @@ def espfilt(obs_archive: Archive, method: str = 'histogram', with_smoothing: Uni
                 raise ValueError("Somehow there is no instance of M1, M2, or PN in that storage key, this should be "
                                  "impossible!")
 
+            # This path is guaranteed to exist, as it was set up in _sas_process_setup. This is where output
+            #  files will be written to.
+            dest_dir = obs_archive.get_processed_data_path(miss, obs_id)
+            ccf_path = dest_dir + 'ccf.cif'
 
-    #         # This path is guaranteed to exist, as it was set up in _sas_process_setup. This is where output
-    #         #  files will be written to.
-    #         dest_dir = obs_archive.get_processed_data_path(miss, obs_id)
-    #         ccf_path = dest_dir + 'ccf.cif'
-    #
-    #         # Set up a temporary directory to work in (probably not really necessary in this case, but will be
-    #         #  in other processing functions).
-    #         temp_name = "tempdir_{}".format(randint(0, 1e+8))
-    #         temp_dir = dest_dir + temp_name + "/"
-    #
-    #         # Checking for the output anom file created by the process (unless turned off with an argument)
-    #         log_name = "{i}{eid}-anom.log".format(i=inst, eid=exp_id)
-    #         final_path = dest_dir + log_name
-    #
-    #         # If it doesn't already exist then we will create commands to generate it
-    #         # TODO Decide whether this is the route I really want to follow for this (see issue #28)
-    #         if not os.path.exists(final_path):
-    #             # Make the temporary directory (it shouldn't already exist but doing this to be safe)
-    #             if not os.path.exists(temp_dir):
-    #                 os.makedirs(temp_dir)
-    #
-    #             # Format the blank command string defined near the top of this function with information
-    #             #  particular to the current mission and ObsID
-    #             cmd = emanom_cmd.format(d=temp_dir, ccf=ccf_path, ef=evt_list_file, of=log_name)
-    #
-    #             # Now store the bash command, the path, and extra info in the dictionaries
-    #             miss_cmds[miss.name][val_id] = cmd
-    #             miss_final_paths[miss.name][val_id] = final_path
-    #             miss_extras[miss.name][val_id] = {}
-    #
-    # # This is just used for populating a progress bar during the process run
-    # process_message = 'Checking for MOS CCD anomalous states'
+            # Set up a temporary directory to work in (probably not really necessary in this case, but will be
+            #  in other processing functions).
+            temp_name = "tempdir_{}".format(randint(0, 1e+8))
+            temp_dir = dest_dir + temp_name + "/"
+
+            #
+            final_path = dest_dir + 'boi.fits'
+
+            # If it doesn't already exist then we will create commands to generate it
+            # TODO Decide whether this is the route I really want to follow for this (see issue #28)
+            if not os.path.exists(final_path):
+                # Make the temporary directory (it shouldn't already exist but doing this to be safe)
+                if not os.path.exists(temp_dir):
+                    os.makedirs(temp_dir)
+
+                # ef_cmd = "cd {d}; export SAS_CCF={ccf}; espfilt eventfile={ef} withoot={woot} ootfile={oot} method={me} " \
+                #          "withsmoothing={ws} smooth={s} withbinning={wb} binsize={bs} ratio={r} withlongnames=yes elow={el} " \
+                         # "ehigh={eh} rangescale={rs} allowsigma={as} limits={ls}"
+
+                # Format the blank command string defined near the top of this function - in this case the
+                #  configuration needs to change depending on the instrument and user configuration
+                if inst == 'PN':
+                    with_oot = 'yes'
+                    rs = range_scale['pn']
+                else:
+                    with_oot = 'yes'
+                    rs = range_scale['mos']
+
+                # Also need to change certain parameters to turn on smoothing or binning if the user wants
+                if smooth_factor is None:
+                    w_sm = 'no'
+
+                cmd = ef_cmd.format(d=temp_dir, ccf=ccf_path, ef=evt_list_file, of=log_name)
+
+                # Now store the bash command, the path, and extra info in the dictionaries
+                miss_cmds[miss.name][val_id] = cmd
+                miss_final_paths[miss.name][val_id] = final_path
+                miss_extras[miss.name][val_id] = {}
+
+    # This is just used for populating a progress bar during the process run
+    process_message = 'Identifying soft-proton flaring in PN and MOS observations'
 
     return miss_cmds, miss_final_paths, miss_extras, process_message, num_cores, disable_progress
