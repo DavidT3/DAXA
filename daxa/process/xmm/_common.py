8b1c08486f2280208a32bab792290ad1d6754e5f
DavidT3 <dt237@sussex.ac.uk>
2023-01-27 17:22:06 -0500

    Updated some stacklevels of warnings

--- daxa/process/xmm/_common.py
+++ daxa/process/xmm/_common.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 27/01/2023, 17:08. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 27/01/2023, 17:22. Copyright (c) The Contributors
 import glob
 import os.path
 from functools import wraps
@@ -45,7 +45,7 @@ def _sas_process_setup(obs_archive: Archive) -> Version:
     else:
         processed = [xm.processed for xm in xmm_miss]
         if any(processed):
-            warn("One or more XMM missions have already been fully processed")
+            warn("One or more XMM missions have already been fully processed", stacklevel=2)
 
     # TODO Remove this when XMM slew is implemented and SAS procedures have been verified as working
     if any([m.name == 'xmm_slew' for m in xmm_miss]):
@@ -177,13 +177,15 @@ def execute_cmd(cmd: str, rel_id: str, miss_name: str, check_path: str, extra_in
     #   https://stackoverflow.com/questions/4789837/how-to-terminate-a-python-subprocess-launched-with-shell-true
     cmd_proc = Popen("exec " + cmd, shell=True, stdout=PIPE, stderr=PIPE)
 
+    print(timeout, '\n')
+
     # This makes sure the process is killed if it does timeout
     try:
         out, err = cmd_proc.communicate(timeout=timeout)
     except TimeoutExpired:
         cmd_proc.kill()
         out, err = cmd_proc.communicate()
-        warn("An XMM process for {} has timed out".format(rel_id))
+        warn("An XMM process for {} has timed out".format(rel_id), stacklevel=2)
 
     # Decodes the stdout and stderr from the binary encoding it currently exists in. The errors='ignore' flag
     #  means that it doesn't throw errors if there is a character it doesn't recognize
