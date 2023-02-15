7e2d3f555a71398d176da0af8109172c5f06cf34
DavidT3 <dt237@sussex.ac.uk>
2023-02-15 14:51:45 -0500

    Added a 'generate' section to the XMM processing toolset - this is where the functions for the generation of basic outputs (images etc.) will be added. They are likely to be different from other DAXA processing steps, as hopefully they will just use XGA. For issue #56

---
+++ daxa/process/xmm/generate.py
@@ -0,0 +1,3 @@
+#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
+#  Last modified by David J Turner (turne540@msu.edu) 15/02/2023, 14:51. Copyright (c) The Contributors
+
