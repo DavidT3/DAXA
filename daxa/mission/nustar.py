076412c1a7d700b1fe434fec2c5be5b4cf3fd3de
DavidT3 <dt237@sussex.ac.uk>
2023-03-07 10:24:55 -0500

    Filled in some of the simple abstract methods, i.e. name, coordinate frame, and the ID regex. For issue #95

--- daxa/mission/nustar.py
+++ daxa/mission/nustar.py
@@ -1,9 +1,9 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 07/03/2023, 09:51. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 07/03/2023, 10:24. Copyright (c) The Contributors
 from typing import List
 
 import pandas as pd
-from astropy.coordinates import BaseRADecFrame
+from astropy.coordinates import BaseRADecFrame, FK5
 
 from daxa import BaseMission
 
@@ -18,15 +18,46 @@ class NuSTAR(BaseMission):
 
     @property
     def name(self) -> str:
-        pass
+        """
+        Property getter for the name of this mission
+
+        :return: The mission name.
+        :rtype: str
+        """
+        # The name is defined here because this is the pattern for this property defined in
+        #  the BaseMission superclass. Suggest keeping this in a format that would be good for a unix
+        #  directory name (i.e. lowercase + underscores), because it will be used as a directory name
+        self._miss_name = "nustar"
+        # This won't be used to name directories, but will be used for things like progress bar descriptions
+        self._pretty_miss_name = "NuSTAR"
+        return self._miss_name
 
     @property
     def coord_frame(self) -> BaseRADecFrame:
-        pass
+        """
+        Property getter for the coordinate frame of the RA-Decs of the observations of this mission.
+
+        :return: The coordinate frame of the RA-Dec.
+        :rtype: BaseRADecFrame
+        """
+        # The name is defined here because this is the pattern for this property defined in
+        #  the BaseMission superclass
+        self._miss_coord_frame = FK5
+        return self._miss_coord_frame
 
     @property
     def id_regex(self) -> str:
-        pass
+        """
+        Property getter for the regular expression (regex) pattern for observation IDs of this mission.
+
+        :return: The regex pattern for observation IDs.
+        :rtype: str
+        """
+        # The ObsID regular expression is defined here because this is the pattern for this property defined in
+        #  the BaseMission superclass - NuSTAR observations have a unique 11-digit ObsID, the construction of
+        #  which is discussed here (https://heasarc.gsfc.nasa.gov/W3Browse/nustar/numaster.html#obsid)
+        self._id_format = '^[0-9]{11}$'
+        return self._id_format
 
     @property
     def all_obs_info(self) -> pd.DataFrame:
