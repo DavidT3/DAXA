fce7953174d3f497a2291546543883b7f5164b4d
DavidT3 <dt237@sussex.ac.uk>
2023-03-07 17:51:09 -0500

    Added explicit imports to the __init__.py of the missions submodule

--- daxa/mission/__init__.py
+++ daxa/mission/__init__.py
@@ -1,8 +1,9 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 15:17. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 07/03/2023, 17:51. Copyright (c) The Contributors
 
-from . import *
-
+from .base import BaseMission
+from .nustar import NuSTARPointed
+from .xmm import XMMPointed
 
 
 
