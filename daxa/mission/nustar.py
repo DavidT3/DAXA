93d706b26dd11ad6274916b4c899a10c67440d13
DavidT3 <dt237@sussex.ac.uk>
2023-03-07 21:30:31 -0500

    Altered the fetch_obs_info function so that it uses the requests module rather than urllib - this is because I'm using requests for the data downloading and its better to just use one module like this I think. For issue #95
    Added the requests module to the requirements.txt

--- daxa/mission/nustar.py
+++ daxa/mission/nustar.py
@@ -1,17 +1,23 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 07/03/2023, 18:24. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 07/03/2023, 21:30. Copyright (c) The Contributors
 import io
+import os
 from datetime import datetime
-from typing import List, Union
-from urllib.request import urlopen
+from multiprocessing import Pool
+from typing import List, Union, Any
+from warnings import warn
 
 import pandas as pd
+import requests
 from astropy.coordinates import BaseRADecFrame, FK5
 from astropy.io import fits
 from astropy.table import Table
 from astropy.time import Time
+from tqdm import tqdm
 
+from daxa import NUM_CORES
 from daxa.mission.base import BaseMission
+from ..exceptions import DAXADownloadError
 
 
 class NuSTARPointed(BaseMission):
@@ -26,14 +32,14 @@ class NuSTARPointed(BaseMission):
 
     def __init__(self, insts: Union[List[str], str] = None):
         """
-        The mission class for pointed NuSTAR observations (i.e. slewing observations are NOT included in the data accessed
-        and collected by instances of this class), nor are observations for which the spacecraft mode was 'STELLAR'.
-        The available observation information is fetched from the HEASArc NuMASTER table, and data are downloaded from
-        the HEASArc https access to their FTP server. Proprietary data are not currently supported by this class.
+        The mission class for pointed NuSTAR observations (i.e. slewing observations are NOT included in the data
+        accessed and collected by instances of this class), nor are observations for which the spacecraft mode was
+        'STELLAR'. The available observation information is fetched from the HEASArc NuMASTER table, and data are
+        downloaded from the HEASArc https access to their FTP server. Proprietary data are not currently supported
+        by this class.
 
         :param List[str]/str insts: The instruments that the user is choosing to download/process data from.
         """
-
         super().__init__()
 
         # Sets the default instruments - both instruments that are on NuSTAR
@@ -174,10 +180,10 @@ class NuSTARPointed(BaseMission):
         fetch_url = host_url + table_head + action + result_max + down_form + fields
 
         # Opening that URL, we can access the results of our request!
-        with urlopen(fetch_url) as urlo:
+        with requests.get(fetch_url, stream=True) as urlo:
             # This opens the data as using the astropy fits interface (using io.BytesIO() to stream it into memory
             #  first so that fits.open can access it as an already opened file handler).
-            with fits.open(io.BytesIO(urlo.read())) as full_fits:
+            with fits.open(io.BytesIO(urlo.content)) as full_fits:
                 # Then convert the data in that fits file just into an astropy table object, and from there to a DF
                 full_nustar = Table(full_fits[1].data).to_pandas()
                 # This cycles through any column with the 'object' data type (string in this instance), and
@@ -248,8 +254,112 @@ class NuSTARPointed(BaseMission):
         self.all_obs_info = rel_nustar
 
     @staticmethod
-    def _download_call(observation_id: str, insts: List[str], level: str, filename: str):
+    def _download_call(observation_id: str, insts: List[str], filename: str):
+        # This two digit code identifies the program type (00 assigned to the first 2-year primary mission, and
+        #  then 01, 02, 03 ... increment for each additional year of observations. Useful here to get to the
+        #  correct directory to find our ObsID
+        prog_id = observation_id[1:3]
+
+        # This identifies the type of source that was being observed, useful here to get to the right directory
+        src_cat = observation_id[0]
+
+
+
+
+        # https://heasarc.gsfc.nasa.gov/FTP/nustar/data/obs/
         pass
 
-    def download(self):
-        pass
+    def download(self, num_cores: int = NUM_CORES):
+        """
+        A method to acquire and download the pointed NuSTAR data that have not been filtered out (if a filter
+        has been applied, otherwise all data will be downloaded). Instruments specified by the chosen_instruments
+        property will be downloaded, which is set either on declaration of the class instance or by passing
+        a new value to the chosen_instruments property.
+
+        :param int num_cores: The number of cores that can be used to parallelise downloading the data. Default is
+            the value of NUM_CORES, specified in the configuration file, or if that hasn't been set then 90%
+            of the cores available on the current machine.
+        """
+
+        # Ensures that a directory to store the 'raw' pointed NuSTAR data in exists - once downloaded and unpacked
+        #  this data will be processed into a DAXA 'archive' and stored elsewhere.
+        if not os.path.exists(self.top_level_path + self.name + '_raw'):
+            os.makedirs(self.top_level_path + self.name + '_raw')
+        # Grabs the raw data storage path
+        stor_dir = self.raw_data_path
+
+        # A very unsophisticated way of checking whether raw data have been downloaded before (see issue #30)
+        #  If not all data have been downloaded there are also secondary checks on an ObsID by ObsID basis in
+        #  the _download_call method
+        if all([os.path.exists(stor_dir + '{o}'.format(o=o)) for o in self.filtered_obs_ids]):
+            self._download_done = True
+
+        if not self._download_done:
+            # If only one core is to be used, then it's simply a case of a nested loop through ObsIDs and instruments
+            if num_cores == 1:
+                with tqdm(total=len(self), desc="Downloading {} data".format(self._pretty_miss_name)) as download_prog:
+                    for obs_id in self.filtered_obs_ids:
+                        # Use the internal static method I set up which both downloads and unpacks the XMM data
+                        self._download_call(obs_id, insts=self.chosen_instruments,
+                                            filename=stor_dir + '{o}'.format(o=obs_id))
+                        # Update the progress bar
+                        download_prog.update(1)
+
+            elif num_cores > 1:
+                raise NotImplementedError('Getting there')
+                # List to store any errors raised during download tasks
+                raised_errors = []
+
+                # This time, as we want to use multiple cores, I also set up a Pool to add download tasks too
+                with tqdm(total=len(self), desc="Downloading {} data".format(self._pretty_miss_name)) \
+                        as download_prog, Pool(num_cores) as pool:
+
+                    # The callback function is what is called on the successful completion of a _download_call
+                    def callback(download_conf: Any):
+                        """
+                        Callback function for the apply_async pool method, gets called when a download task finishes
+                        without error.
+
+                        :param Any download_conf: The Null value confirming the operation is over.
+                        """
+                        nonlocal download_prog  # The progress bar will need updating
+                        download_prog.update(1)
+
+                    # The error callback function is what happens when an exception is thrown during a _download_call
+                    def err_callback(err):
+                        """
+                        The callback function for errors that occur inside a download task running in the pool.
+
+                        :param err: An error that occurred inside a task.
+                        """
+                        nonlocal raised_errors
+                        nonlocal download_prog
+
+                        if err is not None:
+                            # Rather than throwing an error straight away I append them all to a list for later.
+                            raised_errors.append(err)
+                        download_prog.update(1)
+
+                    # Again nested for loop through ObsIDs and instruments
+                    for obs_id in self.filtered_obs_ids:
+                        # Add each download task to the pool
+                        pool.apply_async(self._download_call,
+                                         kwds={'observation_id': obs_id, 'insts': self.chosen_instruments,
+                                               'filename': stor_dir + '{o}'.format(o=obs_id)},
+                                         error_callback=err_callback, callback=callback)
+                    pool.close()  # No more tasks can be added to the pool
+                    pool.join()  # Joins the pool, the code will only move on once the pool is empty.
+
+                # Raise all the download errors at once, if there are any
+                if len(raised_errors) != 0:
+                    raise DAXADownloadError(str(raised_errors))
+
+            else:
+                raise ValueError("The value of NUM_CORES must be greater than or equal to 1.")
+
+            # This is set to True once the download is done, and is used by archives to tell if data have been
+            #  downloaded for a particular mission or not
+            self._download_done = True
+
+        else:
+            warn("The raw data for this mission have already been downloaded.")
