90dfc4f6287abbad95c011b7e004625e1be00978
DavidT3 <dt237@sussex.ac.uk>
2023-03-11 20:26:29 -0500

    Added a chandra section for the Chandra mission class, prior to implementing anything. For issue #115

--- daxa/mission/chandra.py
+++ daxa/mission/chandra.py
@@ -1,12 +1,9 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
 #  Last modified by David J Turner (turne540@msu.edu) 11/03/2023, 20:26. Copyright (c) The Contributors
 
-from .base import BaseMission
-from .chandra import Chandra
-from .nustar import NuSTARPointed
-from .xmm import XMMPointed
+from daxa import BaseMission
 
 
-
-
-
+class Chandra(BaseMission):
+    def __init__(self):
+        pass
