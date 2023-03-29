a54c986d13b2ba1105787e6e2d289ccef3cf9f7f
DavidT3 <dt237@sussex.ac.uk>
2023-03-29 17:07:21 -0400

    Made sure to import things in the process.xmm init so people can import from process.xmm
    Signed-off-by: DavidT3 <dt237@sussex.ac.uk>

--- daxa/process/xmm/__init__.py
+++ daxa/process/xmm/__init__.py
@@ -1,3 +1,7 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 30/11/2022, 15:17. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 29/03/2023, 17:07. Copyright (c) The Contributors
 
+from .assemble import epchain, emchain, cleaned_evt_lists, merge_subexposures
+from .check import emanom
+from .clean import espfilt
+from .setup import cif_build, odf_ingest
