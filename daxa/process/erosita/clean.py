eb394bd8d08eb16c559e23977ff6b3b5b0b45d02
Jessica Pilling <jessicapilling221@gmail.com>
2023-05-02 13:52:11 +0100

    Adding in the esass default arguments into the flaregti function

--- daxa/process/erosita/clean.py
+++ daxa/process/erosita/clean.py
@@ -8,16 +8,18 @@ from daxa.process.erosita._common import _esass_process_setup, ALLOWED_EROSITA_M
 
 # JESS_TODO write an esass call wrapper and then the return and rtype in the docstring
 # JESS_TODO check each argument type is correct
-# JESS_TODO decide on default values
 
-def flaregti(obs_archive: Archive, eventfile: str, gtifile: str, pimin: float, pimax: float,
-mask_pimin: float, mask_pimax: float, xmin: float, xmax: float, ymin: float, ymax: float, gridsize: int,
-binsize: int, detml: int, timebin: int, source_size: int, source_like: int, fov_radius: int, 
-threshold: float, max_threshold: float, write_mask: bool, mask: str, mask_iter: int, write_lightcurve: bool,
-lightcurve: str, write_thresholdimg: bool, thresholdimg: str):
+# DAVID_QUESTION need to discuss which arguments to keep
+
+def flaregti(obs_archive: Archive, pimin: float = 200, pimax: float = 10000, mask_pimin: float = 200, 
+            mask_pimax: float = 10000, xmin: float = -108000, xmax: float = 108000, ymin: float = -108000, 
+            ymax: float = 108000, gridsize: int = 18, binsize: int = 1200, detml: int = 10, 
+            timebin: int = 20, source_size: int = 25, source_like: int = 10, fov_radius: int = 30, 
+            threshold: float = -1, max_threshold: float = -1, write_mask: str = 'yes', mask_iter: int = 3,
+            write_lightcurve: str = 'yes', write_thresholdimg: str = 'no'):
     """
-    The DAXA wrapper for the eROSITA eSASS task flaregti, which attempts to identify good time intervals.
-    This has been tested up to flaregti v.1.20.
+    The DAXA wrapper for the eROSITA eSASS task flaregti, which attempts to identify good time intervals with minimal flaring.
+    This has been tested up to flaregti v1.20.
 
     This function does not generate final event lists, but instead is used to create good-time-interval files
     which are then applied to the creation of final event lists, along with other user-specified filters, in the
@@ -25,9 +27,6 @@ lightcurve: str, write_thresholdimg: bool, thresholdimg: str):
 
     :param Archive obs_archive: An Archive instance containing eROSITA mission instances with observations for
         which flaregti should be run. This function will fail if no eROSITA missions are present in the archive.
-    :param eventfile str: Name of input event file.
-    :param gtifile str: Optional name of output event file. If unset, the FLAREGTI extensions are added to 
-        the input event file.
     :param pimin float:  Lower PI bound of energy range for lightcurve creation.
     :param pimax float:  Upper PI bound of energy range for lightcurve creation.
     :param mask_pimin: Lower PI bound of energy range for finding sources to mask.
@@ -47,12 +46,9 @@ lightcurve: str, write_thresholdimg: bool, thresholdimg: str):
     :param threshold float: Flare threshold; dynamic if negative (unit: counts/deg^2/sec).
     :param max_threshold float: Maximum threshold rate, if positive (unit: counts/deg^2/sec),
         if set this forces the threshold to be this rate or less.
-    :param write_mask bool: Write mask image.
-    :param mask str:  Name of optional output mask image.
+    :param write_mask str: Write mask image.
     :param mask_iter int: Number of repetitions of source masking and GTI creation.
-    :param write_lightcurve bool: Write lightcurve.
-    :param lightcurve str: Name of optional output lightcurve.
-    :param write_thresholdimg bool: Whether to write a FITS threshold image.
-    :param thresholdimg str: Name of threshold image. 
+    :param write_lightcurve str: Write lightcurve.
+    :param write_thresholdimg str: Whether to write a FITS threshold image.
     """
     pass
