9c933837ad9d3ea8e400f7cfaadc0d3c3a480a32
Jessica Pilling <jessicapilling221@gmail.com>
2023-05-03 11:11:38 +0100

    removing some arguments so they arent user controlled

--- daxa/process/erosita/clean.py
+++ daxa/process/erosita/clean.py
@@ -10,18 +10,17 @@ from daxa.archive.base import Archive
 from daxa.exceptions import NoDependencyProcessError
 from daxa.process.erosita._common import _esass_process_setup, ALLOWED_EROSITA_MISSIONS
 
-# JESS_TODO write an esass call wrapper and then the return and rtype in the docstring
+# JESS_TODO write an esass call wrapper 
 # JESS_TODO check each argument type is correct
+# JESS_TODO put arguments as quantities, need to do .value when putting it into cmd line
+#JESS_TODO see what the limits are on xmax, xmin is it the size of a sweep
+#JESS_TODO see how it deals with sweeps vs. pointing
 
-# DAVID_QUESTION need to discuss which arguments to keep
 
 def flaregti(obs_archive: Archive, pimin: float = 200, pimax: float = 10000, mask_pimin: float = 200, 
-            mask_pimax: float = 10000, xmin: float = -108000, xmax: float = 108000, ymin: float = -108000, 
-            ymax: float = 108000, gridsize: int = 18, binsize: int = 1200, detml: int = 10, 
-            timebin: int = 20, source_size: int = 25, source_like: int = 10, fov_radius: int = 30, 
-            threshold: float = -1, max_threshold: float = -1, write_mask: bool = True, mask_iter: int = 3,
-            write_lightcurve: bool = True, write_thresholdimg: bool = False, num_cores: int = NUM_CORES,
-            disable_progress: bool = False, timeout: Quantity = None):
+            mask_pimax: float = 10000, binsize: int = 1200, detml: int = 10, timebin: int = 20, 
+            source_size: int = 25, source_like: int = 10, threshold: float = -1, max_threshold: float = -1,  
+            mask_iter: int = 3, num_cores: int = NUM_CORES, disable_progress: bool = False, timeout: Quantity = None):
     """
     The DAXA wrapper for the eROSITA eSASS task flaregti, which attempts to identify good time intervals with minimal flaring.
     This has been tested up to flaregti v1.20.
@@ -40,14 +39,12 @@ def flaregti(obs_archive: Archive, pimin: float = 200, pimax: float = 10000, mas
     :param float xmax: Sky pixel range for flare analysis: Xmax.
     :param float ymin: Sky pixel range for flare analysis: Ymin.
     :param float ymax: Sky pixel range for flare analysis: Ymax.
-    :param int gridsize: Number of grid points per dimension for dynamic threshold calculation.
     :param int binsize: Bin size of mask image (unit: sky pixels).
     :param int detml: Likelihood threshold for mask creation.
     :param int timebin: Bin size for lightcurve (unit: seconds).
     :param int source_size: Diameter of source extracton area for dynamic threshold calculation (unit: arcsec);
         this is the most important parameter if optimizing for extended sources.
     :param int source_like: Source likelihood for automatic threshold calculation.
-    :param int fov_radius: FoV radius used when computing a dynamic threshold (unit: arcmin).
     :param float threshold: Flare threshold; dynamic if negative (unit: counts/deg^2/sec).
     :param float max_threshold: Maximum threshold rate, if positive (unit: counts/deg^2/sec),
         if set this forces the threshold to be this rate or less.
@@ -88,55 +85,17 @@ def flaregti(obs_archive: Archive, pimin: float = 200, pimax: float = 10000, mas
     if (mask_pimin < 200 or mask_pimin > 10000) or (mask_pimax < 200 or mask_pimax > 10000):
         raise ValueError("The mask_pimin and mask_pimax value must be between 200 eV and 10000 eV.")
 
-    # Checking xmin, xmax, ymin, ymax are valid for eSASS processing
-    if (xmin < -108000 or xmin > 108000) or (xmax < -108000 or xmax > 108000) or \
-       (ymin < -108000 or ymin > 108000) or (ymax < -108000 or ymax > 108000):
-        raise ValueError("The xmin, xmax, ymin, and ymax values must be between -108000 and 108000.")
-    
-    # Checking that the higher pixel limit isn't below the lower limit in the x dimension
-    if (xmax <= xmin):
-        raise ValueError("The xmax argument must be larger than the xmin argument.")
-    
-    # Checking that the higher pixel limit isn't below the lower limit in the y dimension
-    if (ymax <= ymin):
-        raise ValueError("The ymax argument must be larger than the ymin argument.")
-    
-    # Checking user's input for write_thresholdimg is of the correct type
-    if not isinstance(write_thresholdimg, bool):
-        raise TypeError("The write_thresholdimg parameter must be a boolean.")
+    # These parameters we want DAXA to have control over, not the user
+    gridsize = 18   # Sections of the image a source detection is run over to determine a dynamic threshold
+    fov_radius = 30 # Not sure about this parameter yet
+    xmin = -108000  # These are for making the image
+    xmax = 108000
+    ymin = -108000
+    ymax = 108000
+    write_thresholdimg = 'yes'
+    write_mask = 'yes'
+    write_lightcurve = 'yes' 
 
-    # Checking user's input for write_mask is of the correct type
-    if not isinstance(write_mask, bool):
-        raise TypeError("The write_mask parameter must be a boolean.")
-    
-    # Checking user's input for write_lightcurve is of the correct type
-    if not isinstance(write_lightcurve, bool):
-        raise TypeError("The write_lightcurve parameter must be a boolean.")
-    
-    # Need to change parameter to write threshold image if the user wants it. The parameter
-    #  must be changed from boolean to a 'yes' or 'no' string because that is what flaregti wants
-    if write_thresholdimg:
-        write_thresholdimg = 'yes'
-    else:
-        write_thresholdimg = 'no'
-    # Similarly with write_mask
-    if write_mask:
-        write_mask = 'yes'
-    else:
-        write_mask = 'no'
-    # And lastly with write_lightcurve
-    if write_lightcurve:
-        write_lightcurve = 'yes'
-    else:
-        write_lightcurve = 'no'
-
-    # Checking string inputs are valid
-    if write_thresholdimg != 'yes' or 'no':
-        raise ValueError("The string passed for 'write_thresholdimg' must be either yes or no")
-    if write_mask != 'yes' or 'no':
-        raise ValueError("The string passed for 'write_mask' must be either yes or no")
-    if write_lightcurve != 'yes' or 'no':
-        raise ValueError("The string passed for 'write_lightcurve' must be either yes or no")
 
     # Sets up storage dictionaries for bash commands, final file paths (to check they exist at the end), and any
     #  extra information that might be useful to provide to the next step in the generation process
@@ -187,13 +146,12 @@ def flaregti(obs_archive: Archive, pimin: float = 200, pimax: float = 10000, mas
             temp_name = "tempdir_{}".format(randint(0, 1e+8))
             temp_dir = dest_dir + temp_name + "/"
 
-            # DAVID_QUESTION why would you want your image energy limits and lightcurve limits to be different
             # Setting up the paths to the gti file, lightcurve file, threshold image file, and mask image file
             # DAVID_QUESTION not sure which energy lims to use?
-            og_gti_name = "{i}-gti-{l}-{u}.fits".format(i=insts, l=pimin, u=pimax)
+            og_gti_name = "{i}-gti.fits".format(i=insts)
             og_lc_name = "{i}-lc-{l}-{u}.fits".format(i=insts, l=pimin, u=pimax)
             og_thresholdimg_name = "{i}-thresholdimg-{l}-{u}.fits".format(i=insts, l=pimin, u=pimax)
-            og_maskimg_name = "{i}-thresholdimg-{l}-{u}.fits".format(i=insts, l=mask_pimin, u=mask_pimax)
+            og_maskimg_name = "{i}-maskimg-{l}-{u}.fits".format(i=insts, l=mask_pimin, u=mask_pimax)
  
             gti_path = dest_dir + og_gti_name
             lc_path = dest_dir + og_lc_name
@@ -226,7 +184,6 @@ def flaregti(obs_archive: Archive, pimin: float = 200, pimax: float = 10000, mas
                                       ti=threshold_path, omi=og_maskimg_name, mi=maskimg_path)
 
             # Now store the bash command, the path, and extra info in the dictionaries
-            # DAVID_QUESTION not sure on what structure to store erosita data under
             miss_cmds[miss.name][obs_id] = cmd
             miss_final_paths[miss.name][obs_id] = final_paths
             miss_extras[miss.name][obs_id] = {'gti_path': gti_path, 'lc_path': lc_path, 'threshold_path': threshold_path,
