8b5d712614a4a027c702fa986b7c8f57e2d83d3c
Jessica Pilling <jp735@janus.wn.xcat>
2023-06-01 14:56:10 +0100

    unpacking the correct amount of variables from all_obs_info

--- daxa/process/erosita/clean.py
+++ daxa/process/erosita/clean.py
@@ -254,7 +254,7 @@ def flaregti(obs_archive: Archive, pimin: Quantity = Quantity(200, 'eV'), pimax:
         # We iterate through the valid identifying information
         for obs_info in all_obs_info:
             # Split out the information in obs_info
-            obs_id, insts = obs_info
+            obs_id, insts, _ = obs_info
 
             # Search through the process_extra_info attribute of the archive to find the paths 
             #   to the event lists
