731965f2c52b38f26b60c0f3a3d1bc1033f47727
Jessica Pilling <jp735@janus.wn.xcat>
2023-07-19 17:32:44 +0100

    adding in the command creation part of cleaned_evt_lists

--- daxa/process/erosita/assemble.py
+++ daxa/process/erosita/assemble.py
@@ -40,6 +40,9 @@ def cleaned_evt_lists(obs_archive: Archive, lo_en: Quantity = None, hi_en: Quant
         Please note that this is not a timeout for the entire cleaned_evt_lists process, but a timeout for individual
         ObsID-Inst-subexposure processes.
     """
+    # Run the setup for eSASS processes, which checks that eSASS is installed, checks that the archive has at least
+    #  one eROSITA mission in it, and shows a warning if the eROSITA missions have already been processed
+    esass_in_docker = _esass_process_setup(obs_archive)
 
     # Checking user's choice of energy limit parameters
     if not isinstance(lo_en, Quantity) or not isinstance(hi_en, Quantity):
@@ -97,6 +100,102 @@ def cleaned_evt_lists(obs_archive: Archive, lo_en: Quantity = None, hi_en: Quant
     #  Checking user has input a valid pattern
     if (pattern <= 0 or pattern >= 16):
         raise ValueError("Valid eROSITA patterns are between 1 and 15 inclusive")
+    
+    # Converting the parameters to the correct format for the esass command
+    if flag_invert:
+        flag_invert = 'yes'
+    else:
+        flag_invert = 'no'
+
+    # Sets up storage dictionaries for bash commands, final file paths (to check they exist at the end), and any
+    #  extra information that might be useful to provide to the next step in the generation process
+    miss_cmds = {}
+    miss_final_paths = {}
+    miss_extras = {}
+
+    # Just grabs the eROSITA missions, we already know there will be at least one because otherwise _esass_process_setup
+    #  would have thrown an error
+    erosita_miss = [mission for mission in obs_archive if mission.name in ALLOWED_EROSITA_MISSIONS]
+    # We are iterating through erosita missions (options could include erosita_cal_pv for instance).
+    for miss in erosita_miss:
+        # Sets up the top level keys (mission name) in our storage dictionaries
+        miss_cmds[miss.name] = {}
+        miss_final_paths[miss.name] = {}
+        miss_extras[miss.name] = {}
+
+        # This method will fetch the valid data (ObsID, Instruments) that can be processed
+        all_obs_info = obs_archive.get_obs_to_process(miss.name)
+            
+        # Checking that any valid observations are left after the get_obs_to_process function is run
+        if len(all_obs_info) == 0:
+            raise FileNotFoundError("No valid observations have been found, so cleanded_evt_lists may not be run.")
+
+        # all_obs_info is a list of lists, where each list is of the format: [ObsID, Inst, 'usable'].
+        #   There is a new list for each instrument, but I just want to loop over the ObsID in the following bit of code, 
+        #   I also want to know all the instruments that the ObsID contains events for
+        #   So here I am just making a dictionary of the format: {ObsID: insts}
+        # Getting unique obs_ids in all_obs_info
+        obs_ids = list(set([all_obs_info_list[0] for all_obs_info_list in all_obs_info]))
+        obs_info_dict = {}
+        for obs in obs_ids:
+            # Collecting all the insts that a certain ObsID has events for
+            insts = ''.join([all_obs_info_list[1] for all_obs_info_list in all_obs_info if obs in all_obs_info_list])
+            # The insts are all TM{x} where x is a number from 1-7, I just want to retain the x information, 
+            #   and append it to the dict
+            obs_info_dict[obs] = ''.join(ch for ch in insts if ch.isdigit())
+
+        # We iterate through the valid identifying information
+        for obs_id in obs_info_dict:
+            
+            # Checking that flaregti has been run successfully on this observation so that it can be cleaned
+            if not obs_archive.process_success[miss.name]['flaregti'][obs_id]:
+                raise NoProcessingError("Flaregti must be run on {obs} before it may be cleaned.".format(obs_id))
+
+            # Getting the insts associated with this obs for file naming purposes 
+            insts = obs_info_dict[obs_id]
+
+            # Search through the process_extra_info attribute of the archive to find the paths 
+            #   to the event lists
+            evt_list_file = obs_archive._process_extra_info[miss.name][obs_id]['path']
+
+            # This path is guaranteed to exist, as it was set up in _esass_process_setup. This is where output
+            #  files will be written to.
+            dest_dir = obs_archive.get_processed_data_path(miss, obs_id)
+            # Set up a temporary directory to work in (probably not really necessary in this case, but will be
+            #  in other processing functions).
+            temp_name = "tempdir_{}".format(randint(0, 1e+8))
+            temp_dir = dest_dir + temp_name + "/"
+
+            # Setting the paths to the output cleaned event list file
+            filt_evt_name = "{i}-{l}-{u}_clean.fits".format(i=insts, l=lo_en, u=hi_en)
+            filt_evt_path = dest_dir + filt_evt_name
+
+            # If it doesn't already exist then we will create commands to generate it
+            # TODO Need to decide which file to check for here to see whether the command has already been run
+            # Make the temporary directory (it shouldn't already exist but doing this to be safe)
+            if not os.path.exists(temp_dir):
+                os.makedirs(temp_dir)
+
+            evtool_cmd = "cd {d}; evtool eventfiles={ef} gtitype=FLAREGTI outfile={of} flag={f} flag_invert={fi} pattern={p} " \
+                "emin={emin} emax={emax}; mv {of} {fep}; rm -r "
+            
+            cmd = evtool_cmd.format(d=temp_dir, ef=evt_list_file,of=filt_evt_name f=flag fi=flag_invert, p=pattern,
+                emin=lo_en, emax=hi_en, fep=filt_evt_path)
+
+            # Now store the bash command, the path, and extra info in the dictionaries
+            miss_cmds[miss.name][obs_id] = cmd
+            miss_final_paths[miss.name][obs_id] = final_paths
+            miss_extras[miss.name][obs_id] = {'lc_path': lc_path, 'threshold_path': threshold_path,
+                                              'maskimg_path': maskimg_path}
+    
+    # This is just used for populating a progress bar during the process run
+    process_message = 'Cleaning eROSITA observations'
+
+    return miss_cmds, miss_final_paths, miss_extras, process_message, num_cores, disable_progress, timeout, esass_in_docker
+
+            
+
+    
 
     
 
