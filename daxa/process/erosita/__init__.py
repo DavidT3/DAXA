50ef6ae0e0ad45c6fa3835710c6cddf12560172a
Jessica Pilling <jp735@janus.wn.xcat>
2023-07-21 15:42:40 +0100

    importing functions in the __init__

---
+++ daxa/process/erosita/__init__.py
@@ -0,0 +1,5 @@
+#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
+#  Last modified by Jessica Pilling (jp735@sussex.ac.uk) 21/07/2023, 15:38. Copyright (c) The Contributors
+
+from .clean import flaregti
+from .setup import prepare_erositacalpv_info
