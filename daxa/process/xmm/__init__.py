80672f45325cc73e2ae40317d48ecffe877743f9
David Turner <turne540@msu.edu>
2023-08-08 22:38:23 -0400

    Added the rgs functions to the process.xmm init
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/xmm/__init__.py
+++ daxa/process/xmm/__init__.py
@@ -1,7 +1,8 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 29/03/2023, 17:07. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 09/08/2023, 03:38. Copyright (c) The Contributors
 
-from .assemble import epchain, emchain, cleaned_evt_lists, merge_subexposures
+from .assemble import epchain, emchain, cleaned_evt_lists, merge_subexposures, rgs_events, rgs_angles, \
+    cleaned_rgs_event_lists
 from .check import emanom
 from .clean import espfilt
 from .setup import cif_build, odf_ingest
