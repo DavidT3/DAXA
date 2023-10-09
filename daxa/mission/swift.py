285f08f179469cd41acd6c93ebf60d339ad4e9e0
David Turner <turne540@msu.edu>
2023-10-09 13:22:08 -0400

    Removed a leftover print statement from _download of Swift
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/mission/swift.py
+++ daxa/mission/swift.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 09/10/2023, 13:21. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 09/10/2023, 13:22. Copyright (c) The Contributors
 
 import gzip
 import io
@@ -335,8 +335,6 @@ class Swift(BaseMission):
         # This is the path to the HEASArc data directory for this ObsID
         obs_dir = "/FTP/swift/data/obs/{did}/{oid}/".format(did=date_id, oid=observation_id)
         top_url = "https://heasarc.gsfc.nasa.gov" + obs_dir
-
-        print(top_url)
 
         # This opens a session that will persist - then a lot of the next session is for checking that the expected
         #  directories are present.
