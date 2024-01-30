8b0ea10a3764cbc64c83ba277846e225f72a72a3
David Turner <turne540@msu.edu>
2024-01-30 16:13:46 -0500

    Added eRASS1DE class to the mission index
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/mission/__init__.py
+++ daxa/mission/__init__.py
@@ -1,10 +1,10 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 09/10/2023, 23:36. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 30/01/2024, 16:13. Copyright (c) The Contributors
 
 from .asca import ASCA
 from .base import BaseMission
 from .chandra import Chandra
-from .erosita import eROSITACalPV
+from .erosita import eROSITACalPV, eRASS1DE
 from .integral import INTEGRALPointed
 from .nustar import NuSTARPointed
 from .rosat import ROSATAllSky, ROSATPointed
@@ -14,8 +14,9 @@ from .xmm import XMMPointed
 
 # This just links the internal DAXA names of missions to their class
 MISS_INDEX = {'xmm_pointed': XMMPointed, 'nustar_pointed': NuSTARPointed, 'erosita_calpv': eROSITACalPV,
-              'chandra': Chandra, 'rosat_all_sky': ROSATAllSky, 'rosat_pointed': ROSATPointed, 'swift': Swift,
-              'suzaku': Suzaku, 'asca': ASCA, 'integral_pointed': INTEGRALPointed}
+              'erosita_all_sky_de_dr1': eRASS1DE, 'chandra': Chandra, 'rosat_all_sky': ROSATAllSky,
+              'rosat_pointed': ROSATPointed, 'swift': Swift, 'suzaku': Suzaku, 'asca': ASCA,
+              'integral_pointed': INTEGRALPointed}
 
 
 
