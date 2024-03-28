31dd0c47646d2b014bbbb3b7e7f5abbc3680798f
Jessica Pilling <jessicapilling221@gmail.com>
2024-03-28 16:01:36 +0000

    testing chosen instruments

--- tests/test_erosita.py
+++ tests/test_erosita.py
@@ -6,13 +6,22 @@ class TestErosita(unittest.TestCase):
     
     def setUp(self):
 
-        self.mission = eRASS1DE()
+        self.defaults = eRASS1DE()
+        self.tm1 = eRASS1DE(insts='TM1')
     
     def test_chosen_instruments(self):
 
         # error is raised for invalid instruments
         with self.assertRaises(ValueError):
             eRASS1DE(insts='wrong')
+        
+        # error is raised for empty list
+        with self.assertRaises(ValueError):
+            eRASS1DE(insts=[])
+        
+        # Can choose instruments correctly
+        self.assertEqual(self.tm1.chosen_instruments, ['TM1'])
+
 
 
 if __name__ == '__main__':
