e29f0b56bb33cf27a63f94323575e2e77f2d8bcd
David Turner <turne540@msu.edu>
2024-04-05 14:51:18 -0400

    Added a MissionNotASsociatedError
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/exceptions.py
+++ daxa/exceptions.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 11/04/2023, 12:02. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 05/04/2024, 14:51. Copyright (c) The Contributors
 
 
 class DAXAConfigError(Exception):
@@ -342,6 +342,26 @@ class ObsNotAssociatedError(Exception):
             return '{0} '.format(self.message)
         else:
             return 'ObsNotAssociatedError has been raised'
+
+
+class MissionNotAssociatedError(Exception):
+    def __init__(self, *args):
+        """
+        Exception raised if a mission is not associated with a particular archive dataset.
+
+        :param expression:
+        :param message:
+        """
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return '{0} '.format(self.message)
+        else:
+            return 'MissionNotAssociatedError has been raised'
 
 
 class NoRegionsAssociatedError(Exception):
