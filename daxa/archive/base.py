684b91e5b97d5120df63f7a5cb4d08851191b4f0
David Turner <turne540@msu.edu>
2024-04-08 12:05:05 -0400

    Shuffled the get_process_logs contents into an internal method so that it could be shared by other search methods, then added a get_process_raw_error_logs method
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/archive/base.py
+++ daxa/archive/base.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 08/04/2024, 11:53. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 08/04/2024, 12:05. Copyright (c) The Contributors
 import os
 from shutil import rmtree
 from typing import List, Union, Tuple
@@ -847,6 +847,120 @@ class Archive:
 
         return m_name
 
+    def _fetch_matched_log(self, log_struct: dict, process_name: str, mission_name: Union[str, List[str]] = None,
+                           obs_id: Union[str, List[str]] = None, inst: Union[str, List[str]] = None):
+        """
+        This internal method allows for targeted retrieval logs for a specific processing step, but from any of the
+        storage structures for the various types of logs stored by this archive. The storage style is identical,
+        so only one actual retrieval function is necessary.
+        The particular logs retrieved can be narrows down by mission, ObsID, or instrument. Multiple missions,
+        ObsIDs, and instruments may be specified, but only one process at a time. The names of processes that have
+        been run can be found in the 'process_names' property of an Archive.
+
+        :param str process_name: The process for which logs are to be retrieved (see 'process_names' property for
+            the names of processes run on this archive).
+        :param str/List[str] mission_name: The mission name(s) for which logs are to be retrieved. Default is None, in
+            which case all missions will be searched, and either a single name or a list of names can be passed. See
+            'mission_names' for a list of associated mission names.
+        :param str/List[str] obs_id:
+        :param str/List[str] inst:
+        :return: A dictionary containing the requested logs - top level keys are mission names, and the values are
+            lists of logs which match the provided information.
+        :rtype: dict
+        """
+        def unpack_list(to_unpack: list):
+            """
+            A recursive function to go through every layer of a nested list and flatten it all out. It
+            doesn't return anything because to make life easier the 'results' are appended to a variable
+            in the namespace above this one.
+
+            :param list to_unpack: The list that needs unpacking.
+            """
+            # Must iterate through the given list
+            for entry in to_unpack:
+                # If the current element is not a list then all is chill, this element is ready for appending
+                # to the final list
+                if not isinstance(entry, list):
+                    out.append(entry)
+                else:
+                    # If the current element IS a list, then obviously we still have more unpacking to do,
+                    # so we call this function recursively.
+                    unpack_list(entry)
+
+        # If the user has passed a single mission name, we turn it into a list just to make the logic later easier
+        if mission_name is not None and isinstance(mission_name, str):
+            mission_name = [mission_name]
+
+        # If the user has requested a particular mission name(s), then we have to make sure that it is associated
+        #  with the current archive - however if they've just left it as None we don't care, because we'll be
+        #  using every mission in the archive
+        if mission_name is not None and any([mn not in self.mission_names for mn in mission_name]):
+            bad_names = [mn for mn in mission_name if mn not in self.mission_names]
+            raise MissionNotAssociatedError("Some missions ({mn}) are not associated with this "
+                                            "archive; {am} are associated.".format(mn=", ".join(bad_names),
+                                                                                   am=", ".join(self.mission_names)))
+
+        # If the user has specified an ObsID, and it is just a single one, then we turn it into a one-element list
+        #  because it makes the logic easier later on
+        if obs_id is not None and not isinstance(obs_id, str):
+            obs_id = [obs_id]
+
+        # If the user has specified an instrument, and it is just a single one, then we turn it into a one-element list
+        #  because it makes the logic easier later on
+        if inst is not None and isinstance(inst, str):
+            inst = [inst]
+
+        # Logs that match the input will be stored in this dictionary structure
+        matches = {}
+        # Searching for the process_name specified by the user - any matching results will be iterated through (from
+        #  any mission, we filter out the ones we don't want later, if a particular mission has been specified)
+        for res in dict_search(process_name, log_struct):
+            out = []
+            unpack_list(res)
+            # Checking to see whether this result is for a mission the user has specified, if they have specified one
+            if mission_name is None or (mission_name is not None and out[0] in mission_name):
+
+                # We only try to ensure that the instruments are valid if they have been specified!
+                if inst is not None:
+                    # Retrieves the actual mission object for the current iteration
+                    cur_miss = self[out[0]]
+
+                    # We pass in the instrument names to this function, which will make sure they are in the format we
+                    #  need and remove any that can't be identified as matching the mission style
+                    rel_insts = cur_miss.check_inst_names(inst, error_on_bad_inst=False)
+
+                    # Also want to make sure there is a failsafe if none of the instruments were valid
+                    if len(rel_insts) == 0:
+                        rel_insts = None
+                        # Though we shall tell the user if this happens
+                        warn("None of the specified instruments ({i}) were valid for mission "
+                             "{m}.".format(i=", ".join(inst), m=out[0]), stacklevel=2)
+                else:
+                    rel_insts = None
+
+                # Sets up the storage list for the current mission
+                matches[out[0]] = []
+
+                # Iterating through the unique identifiers (may well be an ObsID, but could be ObsID+instrument, or
+                #  ObsID+instrument+subexposure identifier)
+                for ui_res in out[1]:
+                    # This determines just the ObsID from the unique identifier
+                    oi_res = self[out[0]].ident_to_obsid(ui_res)
+
+                    # This checks to see whether the unique identifier was actually just the ObsID, in which case
+                    #  any passed instruments would be pointless, so we wouldn't use them for matching, then whether
+                    #  any of the specified instruments relevant to this mission are present in the unique identifier.
+                    # It isn't elegant, but I think it should suffice
+                    if (oi_res != ui_res and rel_insts is not None and any([ri in ui_res for ri in rel_insts]) and
+                            (obs_id is None or (obs_id is not None and oi_res in obs_id))):
+                        matches[out[0]].append(out[1][ui_res])
+                    # To reach this case either the requested process doesn't use instrument names in its identifier
+                    #  (i.e. it operates on a WHOLE ObsID), or no instruments were specified
+                    elif rel_insts is None and (obs_id is None or (obs_id is not None and oi_res in obs_id)):
+                        matches[out[0]].append(out[1][ui_res])
+
+        return matches
+
     # Then define user-facing methods
     def get_processed_data_path(self, mission: Union[BaseMission, str] = None, obs_id: str = None):
         """
@@ -1180,98 +1294,28 @@ class Archive:
             lists of logs which match the provided information.
         :rtype: dict
         """
-        def unpack_list(to_unpack: list):
-            """
-            A recursive function to go through every layer of a nested list and flatten it all out. It
-            doesn't return anything because to make life easier the 'results' are appended to a variable
-            in the namespace above this one.
+        return self._fetch_matched_log(self.process_logs, process_name, mission_name, obs_id, inst)
 
-            :param list to_unpack: The list that needs unpacking.
-            """
-            # Must iterate through the given list
-            for entry in to_unpack:
-                # If the current element is not a list then all is chill, this element is ready for appending
-                # to the final list
-                if not isinstance(entry, list):
-                    out.append(entry)
-                else:
-                    # If the current element IS a list, then obviously we still have more unpacking to do,
-                    # so we call this function recursively.
-                    unpack_list(entry)
+    def get_process_raw_error_logs(self, process_name: str, mission_name: Union[str, List[str]] = None,
+                                   obs_id: Union[str, List[str]] = None, inst: Union[str, List[str]] = None) -> dict:
+        """
+        This method allows for targeted retrieval of processing raw-error logs (stderr), for a specific processing
+        step. The particular logs retrieved can be narrows down by mission, ObsID, or instrument. Multiple missions,
+        ObsIDs, and instruments may be specified, but only one process at a time. The names of processes that have
+        been run can be found in the 'process_names' property of an Archive.
 
-        # If the user has passed a single mission name, we turn it into a list just to make the logic later easier
-        if mission_name is not None and isinstance(mission_name, str):
-            mission_name = [mission_name]
-
-        # If the user has requested a particular mission name(s), then we have to make sure that it is associated
-        #  with the current archive - however if they've just left it as None we don't care, because we'll be
-        #  using every mission in the archive
-        if mission_name is not None and any([mn not in self.mission_names for mn in mission_name]):
-            bad_names = [mn for mn in mission_name if mn not in self.mission_names]
-            raise MissionNotAssociatedError("Some missions ({mn}) are not associated with this "
-                                            "archive; {am} are associated.".format(mn=", ".join(bad_names),
-                                                                                   am=", ".join(self.mission_names)))
-
-        # If the user has specified an ObsID, and it is just a single one, then we turn it into a one-element list
-        #  because it makes the logic easier later on
-        if obs_id is not None and not isinstance(obs_id, str):
-            obs_id = [obs_id]
-
-        # If the user has specified an instrument, and it is just a single one, then we turn it into a one-element list
-        #  because it makes the logic easier later on
-        if inst is not None and isinstance(inst, str):
-            inst = [inst]
-
-        # Logs that match the input will be stored in this dictionary structure
-        matches = {}
-        # Searching for the process_name specified by the user - any matching results will be iterated through (from
-        #  any mission, we filter out the ones we don't want later, if a particular mission has been specified)
-        for res in dict_search(process_name, self.process_logs):
-            out = []
-            unpack_list(res)
-            # Checking to see whether this result is for a mission the user has specified, if they have specified one
-            if mission_name is None or (mission_name is not None and out[0] in mission_name):
-
-                # We only try to ensure that the instruments are valid if they have been specified!
-                if inst is not None:
-                    # Retrieves the actual mission object for the current iteration
-                    cur_miss = self[out[0]]
-
-                    # We pass in the instrument names to this function, which will make sure they are in the format we
-                    #  need and remove any that can't be identified as matching the mission style
-                    rel_insts = cur_miss.check_inst_names(inst, error_on_bad_inst=False)
-
-                    # Also want to make sure there is a failsafe if none of the instruments were valid
-                    if len(rel_insts) == 0:
-                        rel_insts = None
-                        # Though we shall tell the user if this happens
-                        warn("None of the specified instruments ({i}) were valid for mission "
-                             "{m}.".format(i=", ".join(inst), m=out[0]), stacklevel=2)
-                else:
-                    rel_insts = None
-
-                # Sets up the storage list for the current mission
-                matches[out[0]] = []
-
-                # Iterating through the unique identifiers (may well be an ObsID, but could be ObsID+instrument, or
-                #  ObsID+instrument+subexposure identifier)
-                for ui_res in out[1]:
-                    # This determines just the ObsID from the unique identifier
-                    oi_res = self[out[0]].ident_to_obsid(ui_res)
-
-                    # This checks to see whether the unique identifier was actually just the ObsID, in which case
-                    #  any passed instruments would be pointless, so we wouldn't use them for matching, then whether
-                    #  any of the specified instruments relevant to this mission are present in the unique identifier.
-                    # It isn't elegant, but I think it should suffice
-                    if (oi_res != ui_res and rel_insts is not None and any([ri in ui_res for ri in rel_insts]) and
-                            (obs_id is None or (obs_id is not None and oi_res in obs_id))):
-                        matches[out[0]].append(out[1][ui_res])
-                    # To reach this case either the requested process doesn't use instrument names in its identifier
-                    #  (i.e. it operates on a WHOLE ObsID), or no instruments were specified
-                    elif rel_insts is None and (obs_id is None or (obs_id is not None and oi_res in obs_id)):
-                        matches[out[0]].append(out[1][ui_res])
-
-        return matches
+        :param str process_name: The process for which logs are to be retrieved (see 'process_names' property for
+            the names of processes run on this archive).
+        :param str/List[str] mission_name: The mission name(s) for which logs are to be retrieved. Default is None, in
+            which case all missions will be searched, and either a single name or a list of names can be passed. See
+            'mission_names' for a list of associated mission names.
+        :param str/List[str] obs_id:
+        :param str/List[str] inst:
+        :return: A dictionary containing the requested logs - top level keys are mission names, and the values are
+            lists of logs which match the provided information.
+        :rtype: dict
+        """
+        return self._fetch_matched_log(self.raw_process_errors, process_name, mission_name, obs_id, inst)
 
     def info(self):
         """
