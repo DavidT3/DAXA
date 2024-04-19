87865f4c779798e22164c4cfd75db1ae1309adcc
David Turner <turne540@msu.edu>
2024-04-19 09:20:27 -0400

    Made ASCA work with providing paths!
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/mission/integral.py
+++ daxa/mission/integral.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 08/04/2024, 21:23. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 19/04/2024, 09:20. Copyright (c) The Contributors
 
 import gzip
 import io
@@ -505,6 +505,10 @@ class INTEGRALPointed(BaseMission):
             os.makedirs(self.top_level_path + self.name + '_raw')
         # Grabs the raw data storage path
         stor_dir = self.raw_data_path
+
+        # This INTEGRAL mission currently only supports the downloading of raw data, they don't store anything
+        #  else in their online archive so far as I can tell
+        self._download_type = 'raw'
 
         # A very unsophisticated way of checking whether raw data have been downloaded before (see issue #30)
         #  If not all data have been downloaded there are also secondary checks on an ObsID by ObsID basis in
