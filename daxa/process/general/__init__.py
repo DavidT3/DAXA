6da4eaafd470e627a1ae22bd75e25f34b70d6a23
David Turner <turne540@msu.edu>
2024-04-19 16:42:58 -0400

    Added the 'general' sub-package to process
    Signed-off-by: David Turner <turne540@msu.edu>

---
+++ daxa/process/general/__init__.py
@@ -0,0 +1,4 @@
+#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
+#  Last modified by David J Turner (turne540@msu.edu) 19/04/2024, 16:42. Copyright (c) The Contributors
+
+
