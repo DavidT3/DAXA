5073fde77f68a6f135ce13bbf184e0ebf40a3fea
David Turner <turne540@msu.edu>
2024-04-23 16:33:44 -0400

    Made a nonsense change to eROSITA flaregti function - to test something
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/erosita/clean.py
+++ daxa/process/erosita/clean.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 23/04/2024, 15:24. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 23/04/2024, 16:33. Copyright (c) The Contributors
 import os
 from random import randint
 from typing import Union
@@ -211,7 +211,7 @@ def flaregti(obs_archive: Archive, pimin: Quantity = Quantity(200, 'eV'), pimax:
 
     # These parameters we want DAXA to have control over, not the user
     gridsize = 18   # Sections of the image a source detection is run over to determine a dynamic threshold
-    fov_radius = 30 # Not sure about this parameter yet
+    fov_radius = 30  # Not sure about this parameter yet
     xmin = -108000  # These are for making the image
     xmax = 108000
     ymin = -108000
@@ -226,8 +226,8 @@ def flaregti(obs_archive: Archive, pimin: Quantity = Quantity(200, 'eV'), pimax:
                    "mask_pimin={mpimi} mask_pimax={mpima} xmin={xmi} xmax={xma} ymin={ymi} ymax={yma} " \
                    "gridsize={gs} binsize={bs} detml={dl} timebin={tb} source_size={ss} source_like={sl} " \
                    "fov_radius={fr} threshold={t} max_threshold={mt} write_mask={wm} mask={m} mask_iter={mit} " \
-                   "write_lightcurve={wl} lightcurve={lcf} write_thresholdimg={wti} thresholdimg={tif}; " \
-                   "mv {olc} {lc}; mv {oti} {ti}; mv {omi} {mi}; rm -r {d}"
+                   "write_lightcurve={wl} lightcurve={lcf} write_thresholdimg={wti} thresholdimg={tif} gti_file=el_testo.fits; " \
+                   "mv {olc} {lc}; mv {oti} {ti}; mv {omi} {mi}; "  #rm -r {d}
 
     # Sets up storage dictionaries for bash commands, final file paths (to check they exist at the end), and any
     #  extra information that might be useful to provide to the next step in the generation process
@@ -274,7 +274,6 @@ def flaregti(obs_archive: Archive, pimin: Quantity = Quantity(200, 'eV'), pimax:
 
             # Search through the process_extra_info attribute of the archive to find the paths 
             #   to the event lists
-            # evt_list_file = obs_archive._process_extra_info[miss.name][obs_id]['path']
             evt_list_file = obs_archive[miss.name].get_evt_list_path(obs_id)
 
             # This path is guaranteed to exist, as it was set up in _esass_process_setup. This is where output
@@ -290,9 +289,23 @@ def flaregti(obs_archive: Archive, pimin: Quantity = Quantity(200, 'eV'), pimax:
             og_thresholdimg_name = "{i}-thresholdimg-{l}-{u}.fits".format(i=insts, l=pimin, u=pimax)
             og_maskimg_name = "{i}-maskimg-{l}-{u}.fits".format(i=insts, l=mask_pimin, u=mask_pimax)
  
-            lc_path = dest_dir + og_lc_name
-            threshold_path = dest_dir + og_thresholdimg_name
-            maskimg_path = dest_dir + og_maskimg_name
+            # lc_path = dest_dir + og_lc_name
+            # threshold_path = dest_dir + og_thresholdimg_name
+            # maskimg_path = dest_dir + og_maskimg_name
+
+            # "images/obsid{oi}-inst{i}-subexp{se}-en{l}_{h}keV-image.fits"
+
+            lc_name = "obsid{oi}-inst{i}-subexpNone-en{l}_{h}PI-lightcurve.fits".format(i=insts, l=pimin, h=pimax,
+                                                                                        oi=obs_id)
+            thresholdimg_name = "obsid{oi}-inst{i}-subexpNone-en{l}_{h}PI-thresholdimage.fits".format(i=insts, l=pimin,
+                                                                                                      h=pimax,
+                                                                                                      oi=obs_id)
+            maskimg_name = "obsid{oi}-inst{i}-subexpNone-en{l}_{h}PI-maskimage.fits".format(i=insts, l=mask_pimin,
+                                                                                            h=mask_pimax, oi=obs_id)
+
+            lc_path = os.path.join(dest_dir, 'cleaning', lc_name)
+            threshold_path = os.path.join(dest_dir, 'cleaning', thresholdimg_name)
+            maskimg_path = os.path.join(dest_dir, 'cleaning', maskimg_name)
 
             final_paths = [lc_path, threshold_path, maskimg_path]
 
