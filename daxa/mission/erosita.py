927e884bcb885c888f1b1bc256b27d4380ceb41b
David Turner <turne540@msu.edu>
2024-04-23 10:36:04 -0400

    altered the eROSITACalPV ident_to_obsid to make sure it only returns obsid
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/mission/erosita.py
+++ daxa/mission/erosita.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 23/04/2024, 10:27. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 23/04/2024, 10:36. Copyright (c) The Contributors
 
 import gzip
 import os
@@ -870,8 +870,7 @@ class eROSITACalPV(BaseMission):
 
         :param str ident: The unique identifier used in a particular processing step.
         """
-        # All the eROSITA unique identifiers are just the ObsID, so that's all we return
-        return ident
+        return ident[:6]
 
 
 class eRASS1DE(BaseMission):
