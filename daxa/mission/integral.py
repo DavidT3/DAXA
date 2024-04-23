5cbe2299d8746dbce17446f474645e15448c8dc9
David Turner <turne540@msu.edu>
2024-04-23 10:24:36 -0400

    Added ident_to_obsid method that actually does something to INTEGRAL
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/mission/integral.py
+++ daxa/mission/integral.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 22/04/2024, 22:17. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 23/04/2024, 10:24. Copyright (c) The Contributors
 
 import gzip
 import io
@@ -648,6 +648,8 @@ class INTEGRALPointed(BaseMission):
 
         :param str ident: The unique identifier used in a particular processing step.
         """
-        raise NotImplementedError("The check_process_obs method has not yet been implemented for {n}, as it isn't yet"
-                                  "clear to me what form the unique identifiers will take once we start processing"
-                                  "{n} data ourselves.".format(n=self.pretty_name))
+        # raise NotImplementedError("The check_process_obs method has not yet been implemented for {n}, as it isn't yet"
+        #                           "clear to me what form the unique identifiers will take once we start processing"
+        #                           "{n} data ourselves.".format(n=self.pretty_name))
+        # INTEGRAL ObsIDs (or rather science window IDs) are always 12 digits, so we just retrieve the first 12
+        return ident[:12]
