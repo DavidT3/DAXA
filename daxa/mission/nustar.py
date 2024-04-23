c163652662895875cf2e911290c6d09cf2ce0468
David Turner <turne540@msu.edu>
2024-04-23 10:14:25 -0400

    Added ident_to_obsid method that actually does something to NuSTAR
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/mission/nustar.py
+++ daxa/mission/nustar.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 17/04/2024, 14:35. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 23/04/2024, 10:14. Copyright (c) The Contributors
 import gzip
 import io
 import os
@@ -564,6 +564,8 @@ class NuSTARPointed(BaseMission):
 
         :param str ident: The unique identifier used in a particular processing step.
         """
-        raise NotImplementedError("The check_process_obs method has not yet been implemented for {n}, as it isn't yet"
-                                  "clear to me what form the unique identifiers will take once we start processing"
-                                  "{n} data ourselves.".format(n=self.pretty_name))
+        # raise NotImplementedError("The check_process_obs method has not yet been implemented for {n}, as it isn't yet"
+        #                           "clear to me what form the unique identifiers will take once we start processing"
+        #                           "{n} data ourselves.".format(n=self.pretty_name))
+        # NuSTAR ObsIDs are always 11 digits, so we just retrieve the first 11
+        return ident[:11]
