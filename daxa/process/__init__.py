8c3a8a48acdbd25ca5d97b575eaa5e52ed4763db
David Turner <turne540@msu.edu>
2024-09-03 10:20:16 -0400

    Added entries to the PROC_LOOKUP for all the missions, though they don't have anything in most of them.
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/__init__.py
+++ daxa/process/__init__.py
@@ -1,5 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 02/09/2024, 21:09. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 03/09/2024, 10:20. Copyright (c) The Contributors
 
 from daxa.process.xmm import epchain, emchain, rgs_events, rgs_angles, cleaned_rgs_event_lists, cleaned_evt_lists, \
     merge_subexposures, emanom, espfilt, cif_build, odf_ingest
@@ -26,5 +26,15 @@ PROC_LOOKUP = {'xmm_pointed': {'epchain': epchain,
                             'emanom': emanom,
                             'espfilt': espfilt,
                             'cif_build': cif_build,
-                            'odf_ingest': odf_ingest}
+                            'odf_ingest': odf_ingest},
+               'nustar_pointed': {},
+               'chandra': {},
+               'erosita_calpv': {},
+               'erosita_all_sky_de_dr1': {},
+               'rosat_all_sky': {},
+               'rosat_pointed': {},
+               'swift': {},
+               'suzaku': {},
+               'asca': {},
+               'integral_pointed': {}
                }
