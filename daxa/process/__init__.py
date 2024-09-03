17f0443fd50ce9a6b0c739d42645677a49b26932
David Turner <turne540@msu.edu>
2024-09-03 10:23:33 -0400

    Added the eROSITA mappings of function name to actual function in PROC_LOOKUP. For issue #69
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/__init__.py
+++ daxa/process/__init__.py
@@ -1,6 +1,7 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 03/09/2024, 10:20. Copyright (c) The Contributors
-
+#  Last modified by David J Turner (turne540@msu.edu) 03/09/2024, 10:23. Copyright (c) The Contributors
+from daxa.process.erosita import flaregti
+from daxa.process.erosita.assemble import cleaned_evt_lists as ecleaned_evt_lists
 from daxa.process.xmm import epchain, emchain, rgs_events, rgs_angles, cleaned_rgs_event_lists, cleaned_evt_lists, \
     merge_subexposures, emanom, espfilt, cif_build, odf_ingest
 
@@ -27,10 +28,12 @@ PROC_LOOKUP = {'xmm_pointed': {'epchain': epchain,
                             'espfilt': espfilt,
                             'cif_build': cif_build,
                             'odf_ingest': odf_ingest},
+               'erosita_calpv': {'cleaned_evt_lists': ecleaned_evt_lists,
+                                 'flaregti': flaregti},
+               'erosita_all_sky_de_dr1': {'cleaned_evt_lists': ecleaned_evt_lists,
+                                          'flaregti': flaregti},
                'nustar_pointed': {},
                'chandra': {},
-               'erosita_calpv': {},
-               'erosita_all_sky_de_dr1': {},
                'rosat_all_sky': {},
                'rosat_pointed': {},
                'swift': {},
