50fd2923849610b2e39c671a74edaaeb92dbdbc1
David Turner <turne540@msu.edu>
2024-10-17 17:27:05 -0400

    Added a blankline
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/chandra/assemble.py
+++ daxa/process/chandra/assemble.py
@@ -1,5 +1,6 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 16/10/2024, 22:10. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 17/10/2024, 16:27. Copyright (c) The Contributors
 
 # TODO HERE WE WILL WRITE THE WRAPPER FOR CHANDRA_REPRO AS IT 'ASSEMBLES' THE EXISTING DATA INTO A NEW SHINY
 #  DATASET WITH MODERN CALIBRATION AND PROCESSING APPLIED.
+
