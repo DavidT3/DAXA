9e1093e763a0f3489168fa0e18fdd2311b9983fc
David Turner <turne540@msu.edu>
2024-10-17 17:26:49 -0400

    Added a parse_oif import to the process.chandra init
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/chandra/__init__.py
+++ daxa/process/chandra/__init__.py
@@ -1,2 +1,4 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 16/10/2024, 21:56. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 17/10/2024, 16:26. Copyright (c) The Contributors
+
+from .setup import parse_oif
