14e2f23f73c5821b77dd8f3e8cce68bd1c19b2e8
David Turner <turne540@msu.edu>
2024-10-16 23:10:11 -0400

    Added the basic file structure for the Chandra process section, as I see it now. Should close issue #320
    Signed-off-by: David Turner <turne540@msu.edu>

---
+++ daxa/process/chandra/assemble.py
@@ -0,0 +1,5 @@
+#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
+#  Last modified by David J Turner (turne540@msu.edu) 16/10/2024, 22:10. Copyright (c) The Contributors
+
+# TODO HERE WE WILL WRITE THE WRAPPER FOR CHANDRA_REPRO AS IT 'ASSEMBLES' THE EXISTING DATA INTO A NEW SHINY
+#  DATASET WITH MODERN CALIBRATION AND PROCESSING APPLIED.
---
+++ daxa/process/chandra/clean.py
@@ -0,0 +1,4 @@
+#  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
+#  Last modified by David J Turner (turne540@msu.edu) 16/10/2024, 22:10. Copyright (c) The Contributors
+
+# TODO THIS WILL HOUSE THE FLARE CLEANING FUNCTIONALITY
