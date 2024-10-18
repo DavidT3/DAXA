fda6144d724213afa407cf455fb1ed6c44ae7332
David Turner <turne540@msu.edu>
2024-10-18 17:03:42 -0400

    Started to add the chandra_repro function to daxa.process.chandra.assemble
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/chandra/assemble.py
+++ daxa/process/chandra/assemble.py
@@ -1,6 +1,14 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 17/10/2024, 16:27. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 18/10/2024, 16:03. Copyright (c) The Contributors
 
-# TODO HERE WE WILL WRITE THE WRAPPER FOR CHANDRA_REPRO AS IT 'ASSEMBLES' THE EXISTING DATA INTO A NEW SHINY
-#  DATASET WITH MODERN CALIBRATION AND PROCESSING APPLIED.
+from astropy.units import Quantity
 
+from daxa import NUM_CORES
+from daxa.archive import Archive
+from daxa.process.chandra._common import ciao_call
+
+
+@ciao_call
+def chandra_repro(obs_archive: Archive, num_cores: int = NUM_CORES, disable_progress: bool = False,
+                  timeout: Quantity = None):
+    pass
