f9bdfb1950b8f3225a9e8531f6b844b50ec65fd4
David Turner <turne540@msu.edu>
2024-10-20 22:16:48 -0400

    Barebones setup of chandra_repro is starting to get there - the parts of the archive that identify which ObsIDs can be used and check for the dep runs are now working. For issue #315
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/chandra/assemble.py
+++ daxa/process/chandra/assemble.py
@@ -1,14 +1,17 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 20/10/2024, 20:49. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 20/10/2024, 21:16. Copyright (c) The Contributors
+from random import randint
 
+import numpy as np
 from astropy.units import Quantity
 
 from daxa import NUM_CORES
 from daxa.archive import Archive
-from daxa.process.chandra._common import ciao_call, _ciao_process_setup
+from daxa.exceptions import NoDependencyProcessError
+from daxa.process.chandra._common import _ciao_process_setup
 
 
-@ciao_call
+# @ciao_call
 def chandra_repro(obs_archive: Archive, destreak: bool = True, check_very_faint: bool = False, pix_adj: str = 'default',
                   asol_update: bool = True, grating_pi_filter: bool = True, num_cores: int = NUM_CORES,
                   disable_progress: bool = False, timeout: Quantity = None):
@@ -79,12 +82,57 @@ def chandra_repro(obs_archive: Archive, destreak: bool = True, check_very_faint:
         miss_final_paths[miss.name] = {}
         miss_extras[miss.name] = {}
 
-        rel_obs = obs_archive.get_obs_to_process(miss.name)
-        print(rel_obs)
+        all_obs = obs_archive.get_obs_to_process(miss.name)
+        all_ois = np.array([en[0] for en in all_obs])
 
-        good_obs = obs_archive.check_dependence_success(miss.name, rel_obs, 'prepare_chandra_info',
-                                                        no_success_error=False)
-        print(good_obs)
+        good_obs_sel = obs_archive.check_dependence_success(miss.name, all_ois, 'prepare_chandra_info',
+                                                            no_success_error=False)
+        good_obs = np.array(all_obs)[good_obs_sel]
+
+        # Have to check that there is something for us to work with here!
+        if len(good_obs) == 0:
+            raise NoDependencyProcessError("No observations have had successful 'prepare_chandra_info' runs, so "
+                                           "chandra_repro cannot be run.")
+
+        for obs_info in good_obs:
+            # This is the valid id that allows us to retrieve the specific product for this ObsID-Inst combo
+            val_id = ''.join(obs_info)
+            # Split out the information in obs_info
+            obs_id, inst = obs_info  # exp_id
+
+            print(val_id, obs_id, inst)
+
+            # We need slightly different behaviours for the different instruments, partially because espfilt has
+            #  different instrument naming conventions, and partially because event list files are stored under
+            #  different process names for M1/2 and PN
+            # if inst == 'M1':
+            #     alt_inst = 'mos1'
+            #     evt_list_file = obs_archive.process_extra_info[miss.name]['emchain'][val_id]['evt_list']
+            #     # Of course MOS instruments have no OOT event lists, but I define this here because it does get
+            #     #  fed into the espfilt command later, even if it has no effect
+            #     oot_evt_list_file = 'dataset'
+            # elif inst == 'M2':
+            #     alt_inst = 'mos2'
+            #     evt_list_file = obs_archive.process_extra_info[miss.name]['emchain'][val_id]['evt_list']
+            #     # Of course MOS instruments have no OOT event lists, but I define this here because it does get
+            #     #  fed into the espfilt command later, even if it has no effect
+            #     oot_evt_list_file = 'dataset'
+            # else:
+            #     alt_inst = 'pn'
+            #     evt_list_file = obs_archive.process_extra_info[miss.name]['epchain'][val_id]['evt_list']
+            #     # PN actually does have out of time events, so we grab them from the epchain extra info section
+            #     oot_evt_list_file = obs_archive.process_extra_info[miss.name]['epchain'][val_id]['oot_evt_list']
+
+            # This path is guaranteed to exist, as it was set up in _sas_process_setup. This is where output
+            #  files will be written to.
+            dest_dir = obs_archive.construct_processed_data_path(miss, obs_id)
+
+            # Set up a temporary directory to work in (probably not really necessary in this case, but will be
+            #  in other processing functions).
+            temp_name = "tempdir_{}".format(randint(0, int(1e+8)))
+            temp_dir = dest_dir + temp_name + "/"
+
+            print(temp_dir)
 
 
 
