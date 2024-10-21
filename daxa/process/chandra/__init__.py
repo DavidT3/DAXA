aa1497e12a3b0377916a90200e5b1cbe5f1d85e6
David Turner <turne540@msu.edu>
2024-10-21 11:12:46 -0400

    Imported chandra repro into the init of process.chandra
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/chandra/__init__.py
+++ daxa/process/chandra/__init__.py
@@ -1,4 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 17/10/2024, 22:46. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 21/10/2024, 10:12. Copyright (c) The Contributors
 
+from .assemble import chandra_repro
 from .setup import parse_oif, prepare_chandra_info
