5b659ae5d4121228ec591ead3b0c577ec2af7b74
David Turner <turne540@msu.edu>
2024-10-21 11:11:38 -0400

    The user inputs to chandra_repro are now checked and converted to the style required to set up the command - also said commands should now be generated properly. Ready for a first test run, though checking the expected output files have been created and moving everything into the right output directories is not yet implemented. For issue #315
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/chandra/assemble.py
+++ daxa/process/chandra/assemble.py
@@ -1,5 +1,6 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 20/10/2024, 21:16. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 21/10/2024, 10:11. Copyright (c) The Contributors
+import os
 from random import randint
 
 import numpy as np
@@ -11,7 +12,7 @@ from daxa.exceptions import NoDependencyProcessError
 from daxa.process.chandra._common import _ciao_process_setup
 
 
-# @ciao_call
+@ciao_call
 def chandra_repro(obs_archive: Archive, destreak: bool = True, check_very_faint: bool = False, pix_adj: str = 'default',
                   asol_update: bool = True, grating_pi_filter: bool = True, num_cores: int = NUM_CORES,
                   disable_progress: bool = False, timeout: Quantity = None):
@@ -38,6 +39,10 @@ def chandra_repro(obs_archive: Archive, destreak: bool = True, check_very_faint:
     - 'pi_filter' - for the grating spectra, a low-cost way of lowering the background it seems? I'll leave the
       choice and it'll be on by default
     - I will initially set verbose to 5, to store the maximum amount of data for debugging
+    - The 'root' option, which I think essentially controls the prefix on the generated files, will be set to the
+      ObsID + instrument + randomly generated ID I think, just to be totally sure which ones we made (also while I
+      don't fully understand the behaviours of chandra_repro in practise I think it'll be a good way of tracking what
+      it is doing
 
     :param Archive obs_archive:
     :param bool destreak:
@@ -59,15 +64,56 @@ def chandra_repro(obs_archive: Archive, destreak: bool = True, check_very_faint:
     #  create the command template.
     # Note that we don't need to make a local copy of PFILES here, because that will be added in when the command
     #  is run
-    crp_cmd = ("cd {d}; chandra_repro indir={in_f} outdir={out_f} badpixel='yes' process_events='yes' destreak={ds} "
-               "set_ardlib='no' check_vf_pha={cvf} pix_adj={pa} tf_zo_position='evt2' asol_update={as_up} "
-               "pi_filter={pf} cleanup='no' verbose=5;")
+    crp_cmd = ("cd {d}; chandra_repro indir={in_f} outdir={out_f} root={rn} badpixel='yes' process_events='yes' "
+               "destreak={ds} set_ardlib='no' check_vf_pha={cvf} pix_adj={pa} tf_zo_position='evt2' "
+               "asol_update={as_up} pi_filter={pf} cleanup='no' verbose=5;")
     # "mv *MIEVLI*.FIT ../; mv *ATTTSR*.FIT ../; cd ..; rm -r {d}; mv {oge} {fe}"
+
+
+    # ---------------------------------- Checking and converting user inputs ----------------------------------
+
+    # Make sure that destreak is the right type of object - then we convert to the string 'yes' or 'no' that
+    #  the command line chandra_repro expects
+    if destreak is not bool:
+        raise TypeError("The 'destreak' argument must be a boolean value; you passed {bv}".format(bv=str(destreak)))
+    elif destreak:
+        destreak = 'yes'
+    else:
+        destreak = 'no'
+
+    # Now we do the same thing for the 'check_very_faint' argument, which maps to the 'check_vf_pha' chandra_repro
+    #  parameter - again it is a boolean choice
+    if check_very_faint is not bool:
+        raise TypeError("The 'check_very_faint' argument must be a boolean value; you passed "
+                        "{bv}".format(bv=str(check_very_faint)))
+    elif check_very_faint:
+        check_very_faint = 'yes'
+    else:
+        check_very_faint = 'no'
 
     # The 'pix_adj' argument is not boolean, it has several allowed string values, so we check that the user has
     #  not passed something daft before we blindly pass it on the tool command
     if pix_adj not in ['default', 'edser', 'none', 'randomize']:
         raise ValueError("'pix_adj' must be either; 'default', 'edser', 'none', or 'randomize'.")
+
+    # Rinse and repeat for another boolean variable - 'asol_update'
+    if asol_update is not bool:
+        raise TypeError("The 'asol_update' argument must be a boolean value; you passed "
+                        "{bv}".format(bv=str(asol_update)))
+    elif asol_update:
+        asol_update = 'yes'
+    else:
+        asol_update = 'no'
+
+    # Rinse and repeat for another boolean variable - 'grating_pi_filter'
+    if grating_pi_filter is not bool:
+        raise TypeError("The 'grating_pi_filter' argument must be a boolean value; you passed "
+                        "{bv}".format(bv=str(grating_pi_filter)))
+    elif grating_pi_filter:
+        grating_pi_filter = 'yes'
+    else:
+        grating_pi_filter = 'no'
+    # ---------------------------------------------------------------------------------------------------------
 
     # Sets up storage dictionaries for bash commands, final file paths (to check they exist at the end), and any
     #  extra information
@@ -98,30 +144,10 @@ def chandra_repro(obs_archive: Archive, destreak: bool = True, check_very_faint:
             # This is the valid id that allows us to retrieve the specific product for this ObsID-Inst combo
             val_id = ''.join(obs_info)
             # Split out the information in obs_info
-            obs_id, inst = obs_info  # exp_id
+            obs_id, inst = obs_info  # May also be an exp_id here
 
-            print(val_id, obs_id, inst)
-
-            # We need slightly different behaviours for the different instruments, partially because espfilt has
-            #  different instrument naming conventions, and partially because event list files are stored under
-            #  different process names for M1/2 and PN
-            # if inst == 'M1':
-            #     alt_inst = 'mos1'
-            #     evt_list_file = obs_archive.process_extra_info[miss.name]['emchain'][val_id]['evt_list']
-            #     # Of course MOS instruments have no OOT event lists, but I define this here because it does get
-            #     #  fed into the espfilt command later, even if it has no effect
-            #     oot_evt_list_file = 'dataset'
-            # elif inst == 'M2':
-            #     alt_inst = 'mos2'
-            #     evt_list_file = obs_archive.process_extra_info[miss.name]['emchain'][val_id]['evt_list']
-            #     # Of course MOS instruments have no OOT event lists, but I define this here because it does get
-            #     #  fed into the espfilt command later, even if it has no effect
-            #     oot_evt_list_file = 'dataset'
-            # else:
-            #     alt_inst = 'pn'
-            #     evt_list_file = obs_archive.process_extra_info[miss.name]['epchain'][val_id]['evt_list']
-            #     # PN actually does have out of time events, so we grab them from the epchain extra info section
-            #     oot_evt_list_file = obs_archive.process_extra_info[miss.name]['epchain'][val_id]['oot_evt_list']
+            # Create the variable that points to the 'raw' data for this ObsID of this Chandra mission
+            obs_data_path = miss.raw_data_path + obs_id + '/'
 
             # This path is guaranteed to exist, as it was set up in _sas_process_setup. This is where output
             #  files will be written to.
@@ -129,10 +155,38 @@ def chandra_repro(obs_archive: Archive, destreak: bool = True, check_very_faint:
 
             # Set up a temporary directory to work in (probably not really necessary in this case, but will be
             #  in other processing functions).
-            temp_name = "tempdir_{}".format(randint(0, int(1e+8)))
+            r_id = randint(0, int(1e+8))
+            temp_name = "tempdir_{}".format(r_id)
             temp_dir = dest_dir + temp_name + "/"
 
-            print(temp_dir)
+            # Also make a root prefix for the files output by chandra_repro, with the random int above and
+            #  the ObsID + instrument identifier
+            root_prefix = val_id + "_" + str(r_id)
+
+            # If it doesn't already exist then we will create commands to generate it
+            if ('chandra_repro' not in obs_archive.process_success[miss.name] or
+                    val_id not in obs_archive.process_success[miss.name]['chandra_repro']):
+                # Make the temporary directory for processing - this (along with the temporary PFILES that
+                #  the execute_cmd function will create) should help avoid any file collisions
+                if not os.path.exists(temp_dir):
+                    os.makedirs(temp_dir)
+
+                "cd {d}; chandra_repro indir={in_f} outdir={out_f} root={rn} badpixel='yes' process_events='yes' "
+                "destreak={ds} set_ardlib='no' check_vf_pha={cvf} pix_adj={pa} tf_zo_position='evt2' "
+                "asol_update={as_up} pi_filter={pf} cleanup='no' verbose=5;"
+
+                cmd = crp_cmd.format(d=temp_dir, in_f=obs_data_path, out_f=temp_dir, rn=root_prefix, ds=destreak,
+                                     cvf=check_very_faint, pa=pix_adj, as_up=asol_update, pf=grating_pi_filter)
+
+                # Now store the bash command, the path, and extra info in the dictionaries
+                miss_cmds[miss.name][val_id] = cmd
+                miss_final_paths[miss.name][val_id] = None
+                miss_extras[miss.name][val_id] = {'working_dir': temp_dir}
+
+            # This is just used for populating a progress bar during the process run
+        process_message = 'Finding PN/MOS soft-proton flares'
+
+        return miss_cmds, miss_final_paths, miss_extras, process_message, num_cores, disable_progress, timeout
 
 
 
