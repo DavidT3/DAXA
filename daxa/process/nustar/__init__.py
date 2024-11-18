a3a3f9caf936ebe4139ebcb00d75e3776deeee5b
David Turner <turne540@msu.edu>
2024-11-18 10:43:25 -0500

    Added imports of nupipeline_calibrate and nupipeline_clean to the process.nustar __init__.py file.
    Signed-off-by: David Turner <turne540@msu.edu>

--- daxa/process/nustar/__init__.py
+++ daxa/process/nustar/__init__.py
@@ -1,2 +1,5 @@
 #  This code is a part of the Democratising Archival X-ray Astronomy (DAXA) module.
-#  Last modified by David J Turner (turne540@msu.edu) 07/11/2024, 22:43. Copyright (c) The Contributors
+#  Last modified by David J Turner (turne540@msu.edu) 18/11/2024, 10:43. Copyright (c) The Contributors
+
+from .assemble import nupipeline_calibrate
+from .clean import nupipeline_clean
