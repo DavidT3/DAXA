90bcd2f9351ff3a76dda95ca4b74a4816fe2283d
Jessica Pilling <jp735@sussex.ac.uk>
2025-01-22 17:13:13 +0000

    defining tests

---
+++ tests/test_mission_tools.py
@@ -0,0 +1,25 @@
+import unittest
+
+from daxa.mission.tools import multi_mission_filter_on_positions
+
+class TestMultiMissionFilterOnPositions(unittest.TestCase):
+    def test_dodgy_mission_in_search_dist(self):
+        pass
+    def test_dodgy_search_dist_type(self):
+        pass
+    def test_search_dist_quantity_input(self):
+        pass
+    def test_search_dist_default_input(self):
+        pass
+    def test_dodgy_mission_input(self):
+        pass
+    def test_dodgy_mission_input_within_list(self):
+        pass
+    def test_incorrect_mission_input(self):
+        pass
+    def test_some_missions_given(self):
+        pass
+    def test_some_search_dists_given(self):
+        pass
+
+        
